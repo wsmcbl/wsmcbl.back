787006fdc8fb2b151c1571717d877f05af9d83d5
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-06-26 22:21:23 -0600

    Update: Update wsmcbl.fuction.sql file
    - Add new triggers
    
    Refs: #1, #2

--- database/wsmcbl.fuction.sql
+++ database/wsmcbl.fuction.sql
@@ -26,7 +26,7 @@ BEGIN
            NEW.tariffId,
            NEW.schoolyear,
            case when new.typeid = 1 then NEW.amount*(1 - d.amount) else new.amount end, 
-           case when new.late then (new.amount*0.1) else 0 end,
+           case when new.late then (new.amount*(1 - d.amount)*0.1) else 0 end,
            0,
            false
     FROM Accounting.Student s
@@ -51,7 +51,7 @@ BEGIN
            t.tariffId,
            t.schoolyear,
            case when t.typeid = 1 then t.amount*(1 - d.amount) else t.amount end,
-           case when t.late then t.amount*0.1 else 0.0 end,
+           case when t.late then t.amount*(1 - d.amount)*0.1 else 0.0 end,
            0,
            false
     FROM Accounting.tariff t
@@ -86,6 +86,32 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trg_update_debt_history AFTER INSERT ON Accounting.Transaction_Tariff
     FOR EACH ROW EXECUTE FUNCTION Accounting.update_debt_history();
+
+
+-- Update debt history by tariff overdue--
+CREATE OR REPLACE FUNCTION Accounting.update_debt_history_by_tariff_overdue()
+    RETURNS TRIGGER AS $$
+BEGIN
+    WITH query_aux AS
+        (SELECT s.studentid, d.tariffid, round(new.amount*(1 - disc.amount)) as subtotal, new.late as islate
+              FROM accounting.debthistory d
+                       JOIN accounting.student s on s.studentid = d.studentid
+                       JOIN accounting.discount disc ON disc.discountid = s.discountid
+              WHERE d.tariffid = new.tariffid and new.typeid = 1)
+    
+    UPDATE Accounting.DebtHistory dh
+    SET subamount = q.subtotal,
+        arrear = case when q.islate then round(q.subtotal*0.1) else 0.0 end
+    FROM query_aux as q 
+    where dh.studentid = q.studentid and 
+          dh.tariffid = q.tariffid; 
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_update_debt_history_by_tariff_overdue AFTER update ON Accounting.tariff
+    FOR EACH ROW EXECUTE FUNCTION Accounting.update_debt_history_by_tariff_overdue();
 
 
 
