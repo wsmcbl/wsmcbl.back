53486a0d5d3c87d361ae55dbf432171b8ca65ce9
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-07-18 17:53:04 -0600

    Feat: Add Validator to EnrollmentToCreateDtoValidator class
    - update program
    
    Refs: (#6) #4

---
+++ src/middleware/filter/EnrollmentToCreateDtoValidator.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+using wsmcbl.src.dto.input;
+
+namespace wsmcbl.src.middleware.filter;
+
+public class EnrollmentToCreateDtoValidator : AbstractValidator<EnrollmentToCreateDto>
+{
+    public EnrollmentToCreateDtoValidator()
+    {
+        RuleFor(d => d.gradeId)
+            .NotEmpty()
+            .WithMessage("GradeId must not be empty");
+        RuleFor(d => d.quantity)
+            .GreaterThan(0)
+            .LessThan(8)
+            .WithMessage("Quantity must be between 1 and 7");
+    }
+}
