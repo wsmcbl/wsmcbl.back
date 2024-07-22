fbbe9f26b1663cbaded9ac650d4e74ce1860c538
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-07-22 09:56:03 -0600

    Update: EnrollStudentController.cs class
    Refs: (#13) #15

--- src/controller/business/EnrollStudentController.cs
+++ src/controller/business/EnrollStudentController.cs
@@ -31,9 +31,10 @@ public class EnrollStudentController(DaoFactory daoFactory) : BaseController(dao
     public async Task<StudentEntity> saveEnroll(StudentEntity student, string enrollmentId)
     {
         daoFactory.studentDao!.create(student);
+        await daoFactory.execute();
 
         var academyStudent = new model.academy.StudentEntity();
-        daoFactory.accountingStudentDao!.create(new model.accounting.StudentEntity());
+        daoFactory.academyStudentDao!.create(academyStudent);
         await daoFactory.execute();
 
         return student;
