118d7660b7db9b28333238d71681a81e72266dd4
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-07-22 09:24:59 -0600

    Update: Update EnrollStudentController class
    Refs: (#13) #15

--- src/controller/business/PrintDocumentsController.cs
+++ src/controller/business/PrintDocumentsController.cs
@@ -1,6 +1,8 @@
+using wsmcbl.src.model.dao;
+
 namespace wsmcbl.src.controller.business;
 
-public class PrintDocumentsController : IPrintDocumentsController
+public class PrintDocumentsController(DaoFactory daoFactory) : BaseController(daoFactory), IPrintDocumentsController
 {
     public async Task<byte[]> getEnrollDocument(string studentId)
     {
