d1fd5c2d24d13747714b718aa6fb1dff9856b1eb
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-07-24 10:17:13 -0600

    Update: Add UserControllerTests class
    Refs: (#6) #10

--- tests/integration/CreateOfficialEnrollmentActionsIntegration.cs
+++ tests/integration/CreateOfficialEnrollmentActionsIntegration.cs
@@ -1,10 +1,23 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using wsmcbl.src.controller.api;
 using wsmcbl.tests.utilities;
 
 namespace wsmcbl.tests.integration;
 
-public class CreateOfficialEnrollmentActionsIntegration
+public class CreateOfficialEnrollmentActionsIntegration : WebApplicationFactory<Program>
 {
+    protected override IWebHostBuilder CreateWebHostBuilder()
+    {
+        return base.CreateWebHostBuilder()
+            .UseEnvironment("Test")
+            .UseConfiguration(new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Test.json")
+                .Build());
+    }
+    
     [Fact]
     public async Task getStudentList()
     {
