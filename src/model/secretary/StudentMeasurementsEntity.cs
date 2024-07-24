70af3e22e5e2782850966c737b77e3de9e6c3d8b
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-07-24 13:35:42 -0600

    Update: Update StudentEntity class, add navigations property configurations
    Refs: (#13) #16

--- src/model/secretary/StudentMeasurementsEntity.cs
+++ src/model/secretary/StudentMeasurementsEntity.cs
@@ -3,10 +3,7 @@ namespace wsmcbl.src.model.secretary;
 public class StudentMeasurementsEntity
 {
     public string measurementId { get; set; } = null!;
-
     public string studentId { get; set; } = null!;
-
     public double weight { get; set; }
-
     public int height { get; set; }
 }
