b9c3d2ec2da8b3d6f51639a775ec5a38ea83c9a5
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-07-25 22:52:07 -0600

    Update: Update EnrollStudentActionsTest class
    Refs: (#13) #15 #16

--- src/utilities/HttpStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace wsmcbl.src.utilities;
-
-public enum HttpStatus
-{
-    EntityNotFound = 410,
-    ArgumentInvalid = 460,
-    NotApplied = 461
-}
--- tests/integration/EnrollStudentActionsTest.cs
+++ tests/integration/EnrollStudentActionsTest.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc.Testing;
-using wsmcbl.src.dto;
-using wsmcbl.src.dto.secretary;
 using wsmcbl.src.utilities;
 
 namespace wsmcbl.tests.integration;
@@ -12,21 +10,12 @@ public class EnrollStudentActionsTest : BaseIntegrationTest
     }
     
     [Fact]
-    public async Task Get_Endpoint_Returns_Success()
+    public async Task getStudentList_ShouldReturnOkCode_WhenCalled()
     {
-        var dto = new TariffDataDto
-        {
-            concept = "Nueva tariff, desde las pruebas",
-            amount = 100.0f,
-            dueDate = new DateOnlyDto(2023, 5, 1),
-            typeId = 1,
-            modality = 2
-        };
-        
-        const string uri = "/v1/secretary/configurations/schoolyears/tariffs";
-        var response = await client.PostAsync(uri, getContentByDto(dto));
+        const string uri = "/v1/secretary/enrollments/students";
+        var response = await client.GetAsync(uri);
         
         response.EnsureSuccessStatusCode();
-        Assert.Equal(System.Net.HttpStatusCode.Created, response.StatusCode);
+        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
     }
 }
