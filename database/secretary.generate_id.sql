dcd7512c55429cf0fee83924e7af61d2055bbb5b
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-07-30 16:12:47 -0600

    Update: Update Secretary.Student, Secretary.StudentParent and Secretary.StudentTutor tables
    - Add new function in secretary.generate_id.sql file
    
    Refs: (#13) #14

--- database/secretary.generate_id.sql
+++ database/secretary.generate_id.sql
@@ -43,3 +43,33 @@ BEGIN
     Return 'sub' || seq_part;
 END;
 $$ LANGUAGE plpgsql;
+
+
+-- Generate secretary.parent id
+CREATE SEQUENCE if not exists secretary.parent_id_seq START 10;
+
+CREATE OR REPLACE FUNCTION secretary.generate_parent_id()
+    RETURNS varchar(15) AS $$
+DECLARE
+    seq_part CHAR(3);
+BEGIN
+    seq_part := LPAD(NEXTVAL('secretary.parent_id_seq')::TEXT, 4, '0');
+
+    Return 'par' || seq_part;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Generate secretary.tutor id
+CREATE SEQUENCE if not exists secretary.tutor_id_seq START 10;
+
+CREATE OR REPLACE FUNCTION secretary.generate_tutor_id()
+    RETURNS varchar(15) AS $$
+DECLARE
+    seq_part CHAR(3);
+BEGIN
+    seq_part := LPAD(NEXTVAL('secretary.tutor_id_seq')::TEXT, 4, '0');
+
+    Return 'tur' || seq_part;
+END;
+$$ LANGUAGE plpgsql;
