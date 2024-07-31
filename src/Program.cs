ae6530ae343b0b7e0633ec7e154d8b4155ab2343
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-07-30 19:37:49 -0600

    Feat: Update wsmcbl.src.csproj and program.cs files

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using wsmcbl.src.controller.business;
 using wsmcbl.src.database;
 using wsmcbl.src.database.context;
+using wsmcbl.src.middleware;
 using wsmcbl.src.middleware.filter;
 using wsmcbl.src.model.dao;
 using wsmcbl.src.utilities;
@@ -27,7 +28,7 @@ builder.Services.AddTransient<IEnrollStudentController, EnrollStudentController>
 
 var app = builder.Build();
 
-//app.UseMiddleware<ApiExceptionHandler>();
+app.UseMiddleware<ApiExceptionHandler>();
 
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerUIConfig());
