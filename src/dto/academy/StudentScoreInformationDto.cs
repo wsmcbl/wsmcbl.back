78cfb13078a9c96312f174cebd3522b8f4822a4c
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-08-01 13:53:57 -0600

    Feat: Add ScoreDto and ScoreItemDto classes
    - update StudentScoreInformationDto.cs class
    
    Refs: (#18) #22

--- src/dto/academy/StudentScoreInformationDto.cs
+++ src/dto/academy/StudentScoreInformationDto.cs
@@ -6,11 +6,19 @@ public class StudentScoreInformationDto
 {
     public string studentName { get; set; }
     public string teacherName { get; set; }
-    
+    public string enrollment { get; set; }
+    public List<ScoreDto> scores { get; set; }
 
     public StudentScoreInformationDto(StudentEntity student, TeacherEntity teacher)
     {
         studentName = student.fullName();
         teacherName = teacher.fullName();
+        enrollment = teacher.getEnrollmentLabel();
+
+        scores = [];
+        foreach (var item in student.scores)
+        {
+            scores.Add(new ScoreDto(item));
+        }
     }
 }
