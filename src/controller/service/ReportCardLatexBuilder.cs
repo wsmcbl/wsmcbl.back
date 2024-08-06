5ccebecbb83562b24085f9c79c417c276ed054fe
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-08-05 20:14:11 -0600

    Feat: Update report-card.tex file
    Refs: (#18) #19

--- src/controller/service/ReportCardLatexBuilder.cs
+++ src/controller/service/ReportCardLatexBuilder.cs
@@ -4,8 +4,10 @@ namespace wsmcbl.src.controller.service;
 
 public class ReportCardLatexBuilder : LatexBuilder
 {
+    private readonly string templatesPath;
     public ReportCardLatexBuilder(string templatesPath, string outPath) : base(templatesPath, outPath)
     {
+        this.templatesPath = templatesPath;
     }
 
     protected override string getTemplateName() => "report-card";
@@ -25,9 +27,16 @@ public class ReportCardLatexBuilder : LatexBuilder
             return content;
         }
         
+        content = content.Replace($"\\cbl-logo-wb", $"{templatesPath}/image/cbl-logo-wb.png");
         content = content.Replace($"\\date", DateTime.Today.Date.ToString("dd/MM/yyyy"));
         content = content.Replace($"\\student.name", student.fullName());
         content = content.Replace($"\\teacher.name", teacher.fullName());
+        content = content.Replace($"\\grade", "hola");
+        content = content.Replace($"\\titleLine", "a & b\\\\");
+        content = content.Replace($"\\firstSemester", "r & b\\\\");
+        content = content.Replace($"\\secondSemester", "b & b\\\\");
+        content = content.Replace($"\\finalGrade", "a & a\\\\");
+        content = content.Replace($"\\footLine", "a & w\\\\");
         
         return content;
     }
