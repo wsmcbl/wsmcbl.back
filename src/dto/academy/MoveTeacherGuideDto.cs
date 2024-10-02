08b18214615a2ee1e3b71864a5c7f43681404e80
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-10-02 13:56:56 -0600

    version 2.0.0-BETA

--- src/dto/academy/MoveTeacherGuideDto.cs
+++ src/dto/academy/MoveTeacherGuideDto.cs
@@ -4,6 +4,6 @@ namespace wsmcbl.src.dto.academy;
 
 public class MoveTeacherGuideDto
 {
-    [Required] public string enrollmentId { get; set; }
-    [Required] public string newTeacherId { get; set; }
+    [Required] public string enrollmentId { get; set; } = null!;
+    [Required] public string newTeacherId { get; set; } = null!;
 }
