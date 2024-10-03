d25e5e4dea8f962265a9f67e9de1a74b7c12dff0
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-10-03 14:05:51 -0600

    Update: CreateStudentProfileController classes
    Refs: (#27) #28

--- src/controller/api/CreateStudentProfileActions.cs
+++ src/controller/api/CreateStudentProfileActions.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using wsmcbl.src.controller.business;
+using wsmcbl.src.model.secretary;
 
 namespace wsmcbl.src.controller.api;
 
-[Route("accouting")]
+[Route("accounting")]
 [ApiController]
 public class CreateStudentProfileActions(ICreateStudentProfileController controller) : ControllerBase
 {
-    
+    [HttpPost]
+    [Route("student")]
+    public async Task<IActionResult> createStudent()
+    {
+        StudentEntity student = null!;
+        StudentTutorEntity tutor = null!;
+        var result = await controller.createStudent(student, tutor);
+        return Ok(result);
+    }
 }
