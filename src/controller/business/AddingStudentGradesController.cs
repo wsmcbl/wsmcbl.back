a558f3e52683cc5fb152df5ac3ac7edd3291b4ce
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-10-16 12:26:59 -0600

    Feat: update IAddingStudentGradesController interface
    Refs: (#37) #38

--- src/controller/business/AddingStudentGradesController.cs
+++ src/controller/business/AddingStudentGradesController.cs
@@ -2,5 +2,18 @@ namespace wsmcbl.src.controller.business;
 
 public class AddingStudentGradesController : IAddingStudentGradesController
 {
-    
+    public async Task<object?> getEnrollmentListByTeacherId(string teacherId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<object?> getSubjectList(string teacherId, string enrollmentId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task addGrades(string teacherId, List<string> grades)
+    {
+        throw new NotImplementedException();
+    }
 }
--- src/controller/business/IAddingStudentGradesController.cs
+++ src/controller/business/IAddingStudentGradesController.cs
@@ -2,5 +2,7 @@ namespace wsmcbl.src.controller.business;
 
 public interface IAddingStudentGradesController
 {
-    
+    public Task<object?> getEnrollmentListByTeacherId(string teacherId);
+    public Task<object?> getSubjectList(string teacherId, string enrollmentId);
+    public Task addGrades(string teacherId, List<string> grades);
 }
