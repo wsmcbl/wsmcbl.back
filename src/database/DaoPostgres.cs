0aa23f0abe146e7a52a4fa0ec4afccffb510bc24
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-11-03 10:50:12 -0600

    Update: DaoPostgres class
    Refs: (#37) #39

--- src/database/DaoPostgres.cs
+++ src/database/DaoPostgres.cs
@@ -41,20 +41,24 @@ public class SubjectDaoPostgres(PostgresContext context) : GenericDaoPostgres<Su
 }
 
 
-public class SubjectPartialDaoPostgres(PostgresContext context) : GenericDaoPostgres<SubjectPartialEntity, int>(context), ISubjectPartialDao
+public class SubjectPartialDaoPostgres(PostgresContext context) : 
+    GenericDaoPostgres<SubjectPartialEntity, int>(context), ISubjectPartialDao
 {
     public async Task<List<SubjectPartialEntity>> getListByTeacherAndEnrollment(SubjectPartialEntity subjectPartial)
     {
         var result = await entities
-            .Where(e => e.teacherId == subjectPartial.teacherId && e.enrollmentId == subjectPartial.enrollmentId)
+            .Where(e => e.teacherId == subjectPartial.teacherId 
+                        && e.enrollmentId == subjectPartial.enrollmentId
+                        && e.partialId == subjectPartial.partialId)
             .Include(e => e.gradeList)
             .ToListAsync();
 
         if (result.Count == 0)
         {
             throw new NotFoundException(
-                $"There are no records of (SubjectPartialEntity) for the teacherId ({subjectPartial.teacherId})" +
-                $" and enrollmentId ({subjectPartial.enrollmentId}).");
+                $"There are no records of (SubjectPartialEntity) for the teacherId ({subjectPartial.teacherId})," +
+                $" enrollmentId ({subjectPartial.enrollmentId})" +
+                $" and partialId ({subjectPartial.partialId}).");
         }
 
         return result;
