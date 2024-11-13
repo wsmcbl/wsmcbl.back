49fdd56352e069d708109f627294a037c8e98aa5
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-11-13 00:04:12 -0600

    Feat: update enroll-sheet file
    - update EnrollShettLatexBuilder class
    
    Refs: #44

--- src/controller/service/EnrollSheetLatexBuilder.cs
+++ src/controller/service/EnrollSheetLatexBuilder.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using wsmcbl.src.model.secretary;
 
 namespace wsmcbl.src.controller.service;
@@ -12,7 +13,7 @@ public class EnrollSheetLatexBuilder : LatexBuilder
     {
         this.templatesPath = templatesPath;
         this.entity = entity;
-        isRepetitive = false;
+        isNewEnroll = true;
     }
 
     public void setGrade(string grade)
@@ -23,32 +24,39 @@ public class EnrollSheetLatexBuilder : LatexBuilder
 
     protected override string updateContent(string content)
     {
-        content = content.Replace($"\\cbl-logo-wb", $"{templatesPath}/image/cbl-logo-wb.png");
-        content = content.Replace($"\\date", DateTime.Today.Date.ToString("dd/MM/yyyy"));
-        content = content.Replace($"\\name", entity.fullName());
-        content = content.Replace($"\\grade", grade);
-        content = content.Replace($"\\is.repetitive", getTextByBool(isRepetitive));
-        content = content.Replace($"\\age", getAge(entity.birthday));
-        content = content.Replace($"\\sex", getTextBySex(entity.sex));
-        content = content.Replace($"\\birthday.day", entity.birthday.Day.ToString());
-        content = content.Replace($"\\birthday.month", entity.birthday.Month.ToString());
-        content = content.Replace($"\\birthday.year", entity.birthday.Year.ToString());
-        content = content.Replace($"\\tutor", entity.tutor.name);
-        content = content.Replace($"\\diseases", entity.diseases);
-        content = content.Replace($"\\phones", entity.tutor.phone);
-        content = content.Replace($"\\religion", entity.religion);
-        content = content.Replace($"\\address", entity.address);
+        var today = DateOnly.FromDateTime(DateTime.Today);
+        content = content.Replace($"logo.value", $"{templatesPath}/image/cbl-logo-wb.png");
+        content = content.Replace($"today.value", getDate(today));
+        content = content.Replace($"student.name.value", entity.fullName().ToUpper());
+        content = content.Replace($"degree.value", grade.ToUpper());
+        content = content.Replace($"enroll.record.value", getTextByEnrollRecord().ToUpper());
+        content = content.Replace($"student.age.value", getAge(entity.birthday).ToUpper());
+        content = content.Replace($"student.sex.value", getTextBySex(entity.sex).ToUpper());
+        content = content.Replace($"student.birthday.value", getDate(entity.birthday, false).ToUpper());
+        content = content.Replace($"tutor.value", entity.tutor.name.ToUpper());
+        content = content.Replace($"diseases.value", entity.diseases.ToUpper());
+        content = content.Replace($"phones.value", entity.tutor.phone);
+        content = content.Replace($"email.value", "");
+        content = content.Replace($"religion.value", entity.religion.ToUpper());
+        content = content.Replace($"address.value", entity.address.ToUpper());
 
         content = setParents(content, entity.parents);
         content = setFile(content, entity.file!);
         
-        var today = DateOnly.FromDateTime(DateTime.Today);
-        content = content.Replace($"\\current.year", today.Year.ToString());
+        content = content.Replace($"current.year.value", today.Year.ToString());
         
         return content;
     }
+    
+    private  string getDate(DateOnly date, bool withDay = true)
+    {
+        var culture = new CultureInfo("es-ES");
 
-    public bool isRepetitive { get; set; }
+        var format = withDay ? "dddd dd/MMM/yyyy" : "dd/MMMM/yyyy";
+        return date.ToString(format, culture);
+    }
+
+    private bool isNewEnroll { get; set; }
 
     private static string setParents(string content, ICollection<StudentParentEntity>? entityParents)
     {
@@ -70,36 +78,37 @@ public class EnrollSheetLatexBuilder : LatexBuilder
 
     private static string setParentNull(string content, string typeParent)
     {
-        content = content.Replace($"\\{typeParent}.name", "");
-        content = content.Replace($"\\{typeParent}.idcard", "");
-        content = content.Replace($"\\{typeParent}.occupation", "");
+        content = content.Replace($"{typeParent}.name.value", "");
+        content = content.Replace($"{typeParent}.idcard.value", "");
+        content = content.Replace($"{typeParent}.occupation.value", "");
 
         return content;
     }
     
     private static string setParent(string content, string typeParent, StudentParentEntity parent)
     {
-        content = content.Replace($"\\{typeParent}.name", parent.name);
-        content = content.Replace($"\\{typeParent}.idcard", parent.idCard);
-        content = content.Replace($"\\{typeParent}.occupation", parent.occupation);
+        content = content.Replace($"{typeParent}.name.value", parent.name.ToUpper());
+        content = content.Replace($"{typeParent}.idcard.value", parent.idCard.ToUpper());
+        content = content.Replace($"{typeParent}.occupation.value", parent.occupation.ToUpper());
 
         return content;
     }
 
     private static string setFile(string content, StudentFileEntity file)
     {
-        content = content.Replace($"\\birth.document", getTextByBool(file.birthDocument));
-        content = content.Replace($"\\conduct.document", getTextByBool(file.conductDocument));
-        content = content.Replace($"\\financial.solvency", getTextByBool(file.financialSolvency));
-        content = content.Replace($"\\parent.idcard", getTextByBool(file.parentIdentifier));
-        content = content.Replace($"\\transfer.sheet", getTextByBool(file.transferSheet));
-        content = content.Replace($"\\updated.grade.report", getTextByBool(file.updatedDegreeReport));
+        content = content.Replace($"birth.document.value", getTextByBool(file.birthDocument));
+        content = content.Replace($"conduct.document.value", getTextByBool(file.conductDocument));
+        content = content.Replace($"financial.solvency.value", getTextByBool(file.financialSolvency));
+        content = content.Replace($"parent.idcard.value", getTextByBool(file.parentIdentifier));
+        content = content.Replace($"transfer.sheet.value", getTextByBool(file.transferSheet));
+        content = content.Replace($"updated.grade.report.value", getTextByBool(file.updatedDegreeReport));
 
         return content;
     }
 
     private static string getTextByBool(bool isSomething) => isSomething ? "Sí" : "No";
     private static string getTextBySex(bool sex) => sex ? "Hombre" : "Mujer";
+    private string getTextByEnrollRecord() => isNewEnroll ? "Primer ingreso" : "Reingreso";
     
     private static string getAge(DateOnly birthday)
     {
@@ -112,6 +121,6 @@ public class EnrollSheetLatexBuilder : LatexBuilder
             age--;
         }
 
-        return age.ToString();
+        return $"{age.ToString()} años";
     }
 }
