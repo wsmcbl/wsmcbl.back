9ad2a3c75b19af19164af5809d57d696acc3d2f2
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-11-17 20:56:00 -0600

    Feat: update UserDto class
    Refs: #51

--- src/dto/config/UserDto.cs
+++ src/dto/config/UserDto.cs
@@ -1,12 +1,10 @@
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 using wsmcbl.src.model.config;
 
 namespace wsmcbl.src.dto.config;
 
 public class UserDto
 {
-    [JsonRequired] public int roleId { get; set; }
     [Required] public string name { get; set; }
     public string? secondName { get; set; }
     [Required] public string surname { get; set; }
@@ -21,7 +19,6 @@ public class UserDto
     
     public UserDto(UserEntity user)
     {
-        roleId = user.roleId;
         name = user.name;
         secondName = user.secondName;
         surname = user.surname;
@@ -35,7 +32,6 @@ public class UserDto
     {
         return new UserEntity
         {
-            roleId = roleId,
             name = name,
             secondName = secondName,
             surname = surname,
