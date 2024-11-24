4f60411b9b23931827bfdc0f63c3d364ca55d5f4
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-11-24 16:15:14 -0600

    Feat: update ReportByDateDto and TransactionReportDto classes
    Refs: (#54) #56

--- src/dto/accounting/ReportByDateDto.cs
+++ src/dto/accounting/ReportByDateDto.cs
@@ -1,3 +1,5 @@
+using wsmcbl.src.model.accounting;
+
 namespace wsmcbl.src.dto.accounting;
 
 public class ReportByDateDto
@@ -6,10 +8,15 @@ public class ReportByDateDto
     public DateTime consultedIn { get; set; }
     public DateTime start { get; set; }
     public DateTime end { get; set; }
-    public int totalQuantity { get; set; }
-    public double totalAmount { get; set; }
     public int validQuantity { get; set; }
-    public double validAmount { get; set; }
+    public double validTotal { get; set; }
+    public int invalidQuantity { get; set; }
+    public double invalidTotal { get; set; }
 
-    public List<TransactionReportDto> transactionList { get; set; } = null!;
+    public List<TransactionReportDto> transactionList { get; set; }
+
+    public ReportByDateDto(IEnumerable<(TransactionEntity transaction, StudentEntity student)> list)
+    {
+        transactionList = list.mapToListDto();
+    }
 }
