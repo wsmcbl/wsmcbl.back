db4fe5d954047e4dd4feec239231c49aeefe5f20
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-11-24 15:30:58 -0600

    Feat: update getReportByDate method in TransactionReportByDateActions class
    Refs: (#54) #55

--- src/controller/api/TransactionReportByDateActions.cs
+++ src/controller/api/TransactionReportByDateActions.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using wsmcbl.src.controller.business;
+using wsmcbl.src.exception;
 using wsmcbl.src.middleware;
 
 namespace wsmcbl.src.controller.api;
@@ -10,14 +11,20 @@ namespace wsmcbl.src.controller.api;
 public class TransactionReportByDateActions(ITransactionReportByDateController controller) : ActionsBase
 {
     /// <summary>
-    /// Return the list of 
+    /// Returns summary list of transactions and revenues by date.
     /// </summary>
+    /// <param name="q">The value must be 1, 2, 3 or 4. Value corresponding to today, yesterday, monthly and yearly.</param>
     /// <response code="200">Returns a list, the list can be empty.</response>
     [HttpGet]
-    [Route("transactions/report")]
-    public async Task<IActionResult> getReportByDate()
+    [Route("transactions/revenues")]
+    public async Task<IActionResult> getReportByDate([FromQuery] int q)
     {
-        var list = await controller.getReportByDate();
+        if (q is < 1 or > 4)
+        {
+            throw new IncorrectDataBadRequestException("The query value must be 1,2,3 or 4.");
+        }
+        
+        var list = await controller.getReportByDate(q);
         return Ok(list);
     }
 }
--- src/controller/business/TransactionReportByDateController.cs
+++ src/controller/business/TransactionReportByDateController.cs
@@ -4,7 +4,7 @@ namespace wsmcbl.src.controller.business;
 
 public class TransactionReportByDateController : ITransactionReportByDateController
 {
-    public Task<List<TransactionEntity>> getReportByDate()
+    public Task<List<TransactionEntity>> getReportByDate(int range)
     {
         throw new NotImplementedException();
     }
