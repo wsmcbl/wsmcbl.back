cb15dad5d09ee503fed7bdd58cc4789887a8cc6d
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-11-26 12:27:44 -0600

    Update: getDateRange method in TransactionReportByDateController class
    Refs: (#54) #55

--- src/controller/business/TransactionReportByDateController.cs
+++ src/controller/business/TransactionReportByDateController.cs
@@ -26,28 +26,27 @@ public class TransactionReportByDateController(DaoFactory daoFactory) : BaseCont
         return result.getAlias();
     }
 
-    private DateTime start = DateTime.Now;
-    private DateTime end = DateTime.Now;
+    private DateTime start { get; set; }
+    private DateTime end { get; set; }
 
     public (DateTime start, DateTime end) getDateRange(int range)
     {
-        end = DateTime.Now;
-        start = new DateTime(end.Year, end.Month, end.Day, 0, 0, 0);
-
-        var today = DateTime.Today;
-        switch (range)
+        var now = DateTime.UtcNow;
+        
+        start = now.Date;
+        end = now;
+        
+        switch (range)  
         {
             case 2:
-                start = new DateTime(today.Year, today.Month, today.Day, 0, 0, 0);
-                end = new DateTime(today.Year, today.Month, today.Day, 23, 59, 59);
+                start = start.AddDays(-1);
+                end = end.Date.AddHours(6).AddSeconds(-1);
                 break;
             case 3:
-                start = new DateTime(today.Year, today.Month, 1, 0, 0, 0);
-                end = new DateTime(today.Year, today.Month, end.Day, end.Hour, end.Minute, end.Second);
+                start = new DateTime(now.Year, now.Month, 1);
                 break;
-            default:
-                start = new DateTime(today.Year, 1, 1, 0, 0, 0);
-                end = new DateTime(today.Year, today.Month, end.Day, end.Hour, end.Minute, end.Second);
+            case 4:
+                start = new DateTime(now.Year, 1, 1);
                 break;
         }
 
