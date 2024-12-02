c331347a3eba824bb42661aa7e866ef2db1080a8
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-12-01 23:11:39 -0600

    Feat: update changeStudentEnrollment method in MoveStudentFromEnrollmentController class
    Refs: (#64) #65

--- src/controller/business/MoveStudentFromEnrollmentController.cs
+++ src/controller/business/MoveStudentFromEnrollmentController.cs
@@ -16,6 +16,25 @@ public class MoveStudentFromEnrollmentController(DaoFactory daoFactory)
             throw new EntityNotFoundException("student", studentId);
         }
 
+        if (student.enrollmentId == enrollmentId)
+        {
+            throw new ConflictException("The student is already in this enrollment.");
+        }
+
+        var enrollment = await daoFactory.enrollmentDao!.getById(enrollmentId);
+        if (enrollment == null)
+        {
+            throw new EntityNotFoundException("enrollment", enrollmentId);
+        }
+        
+        var oldEnrollment = await daoFactory.enrollmentDao!.getById(student.enrollmentId!);
+        oldEnrollment!.quantity--;
+        
+        student.enrollmentId = enrollmentId;
+        enrollment.quantity++;
+
+        await daoFactory.execute();
+
         return student;
     }
 }
