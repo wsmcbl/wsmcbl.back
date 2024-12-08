7078ee28512a9f945154b1d41c254b3df12ee0c8
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-12-07 20:36:02 -0600

    Feat: add getTeacherList and getTeacherById methods in MoveTeacherFromSubjectController class
    Refs: (#73) #74

--- src/controller/api/MoveTeacherFromSubjectActions.cs
+++ src/controller/api/MoveTeacherFromSubjectActions.cs
@@ -8,19 +8,35 @@ namespace wsmcbl.src.controller.api;
 [ResourceAuthorizer("admin","secretary")]
 [Route("academy")]
 [ApiController]
-public class MoveTeacherFromSubjectActions(MoveTeacherFromSubjectController fromSubjectController) : ControllerBase
+public class MoveTeacherFromSubjectActions(MoveTeacherFromSubjectController controller) : ControllerBase
 {
+    /// <summary>
+    ///  Returns the list of active teacher
+    /// </summary>
+    /// <response code="200">Returns a list, the list can be empty.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    [HttpGet]
+    [Route("teachers")]
+    public async Task<IActionResult> getTeacherList()
+    {
+        var list = await controller.getTeacherList();
+        return Ok(list.mapListToDto());
+    }
+    
     /// <summary>
     /// Update the teacher of the enrollment.
     /// </summary>
     /// <response code="200">Returns the edited resource.</response>
     /// <response code="400">If the dto is not valid.</response>
-    /// <response code="404">If the enrollment or teacher does not exist.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    /// <response code="404">If the teacher or subject does not exist.</response>
     [HttpPut]
     [Route("teachers")]
     public async Task<IActionResult> updateTeacherEnrollment(MoveTeacherGuideDto dto)
     {
-        await fromSubjectController.updateTeacherEnrollment(dto.newTeacherId);
+        await controller.updateTeacherEnrollment(dto.newTeacherId);
         return Ok();
     }
 }
--- src/controller/business/MoveTeacherFromSubjectController.cs
+++ src/controller/business/MoveTeacherFromSubjectController.cs
@@ -1,7 +1,20 @@
+using wsmcbl.src.model.academy;
+using wsmcbl.src.model.dao;
+
 namespace wsmcbl.src.controller.business;
 
-public class MoveTeacherFromSubjectController
+public class MoveTeacherFromSubjectController(DaoFactory daoFactory) : BaseController(daoFactory)
 {
+    public async Task<List<TeacherEntity>> getTeacherList()
+    {
+        return await daoFactory.teacherDao!.getAll();
+    }
+    
+    public async Task<TeacherEntity?> getTeacherById(string teacherId)
+    {
+        return await daoFactory.teacherDao!.getById(teacherId);
+    }
+    
     public Task updateTeacherEnrollment(string teacherId)
     {
         throw new NotImplementedException();
