222cba91b10e03ef5734ccbc2120c603d70d815a
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-12-08 13:07:30 -0600

    Refactor: add documentation in PrintReportCardByStudentActions and ViewGradeOnlineActions classes
    Refs: (#78) #79

--- src/controller/api/ViewGradeOnlineActions.cs
+++ src/controller/api/ViewGradeOnlineActions.cs
@@ -7,6 +7,13 @@ namespace wsmcbl.src.controller.api;
 [ApiController]
 public class ViewGradeOnlineActions(ViewGradeOnlineController controller) : ControllerBase
 {
+    /// <summary>
+    ///  Return true or false if the student is valid.
+    /// </summary>
+    /// <response code="200">Returns a resource.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    /// <response code="404">Student not found.</response>
     [HttpGet]
     [Route("students/{studentId}")]
     public async Task<IActionResult> validateStudent(string studentId, [FromQuery] string token)
