b4510b74234029a011829aa4aab222d0da32b1d8
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-12-08 19:11:23 -0600

    Feat: add GradeReportLatexBuilder class
    - update DocumentMaker class
    
    Refs: (#78) #79

--- src/controller/service/DocumentMaker.cs
+++ src/controller/service/DocumentMaker.cs
@@ -20,8 +20,7 @@ public class DocumentMaker(DaoFactory daoFactory) : PdfMaker
         var partials = await daoFactory.partialDao!.getListByStudentId(studentId);
         student.setPartials(partials);
         
-        var latexBuilder = new ReportCardLatexBuilder
-            .Builder(resource, $"{resource}/out")
+        var latexBuilder = new ReportCardLatexBuilder.Builder(resource,$"{resource}/out")
             .withStudent(student)
             .withTeacher(teacher)
             .withDegree(enrollment!.label)
@@ -99,6 +98,27 @@ public class DocumentMaker(DaoFactory daoFactory) : PdfMaker
 
     public async Task<byte[]> getGradeReportByStudent(string studentId)
     {
-        return await getReportCardByStudent(studentId);
+        var student = await daoFactory.academyStudentDao!.getByIdInCurrentSchoolyear(studentId);
+        var enrollment = await daoFactory.enrollmentDao!.getById(student.enrollmentId!);
+        var teacher = await daoFactory.teacherDao!.getByEnrollmentId(student.enrollmentId!);
+
+        if (teacher == null)
+        {
+            throw new EntityNotFoundException($"Teacher with enrollmentId ({student.enrollmentId}) not found.");
+        }
+            
+        var partials = await daoFactory.partialDao!.getListByStudentId(studentId);
+        student.setPartials(partials);
+        
+        var latexBuilder = new GradeReportLatexBuilder.Builder(resource,$"{resource}/out")
+            .withStudent(student)
+            .withTeacher(teacher)
+            .withDegree(enrollment!.label)
+            .withSubjects(await getSubjectSort(student.enrollmentId!))
+            .withSemesters(await daoFactory.semesterDao!.getAllOfCurrentSchoolyear())
+            .build();
+        
+        setLatexBuilder(latexBuilder);
+        return getPDF();
     }
 }
---
+++ src/controller/service/GradeReportLatexBuilder.cs
@@ -0,0 +1,161 @@
+using wsmcbl.src.model.academy;
+
+namespace wsmcbl.src.controller.service;
+
+public class GradeReportLatexBuilder(string templatesPath, string outPath) : LatexBuilder(templatesPath, outPath)
+{
+    private StudentEntity student = null!;
+    private TeacherEntity teacher = null!;
+    private List<SemesterEntity> _semesters = null!;
+    private List<(string initials, string subjectId)> subjects = null!;
+    
+    private string degree = null!; 
+    
+    protected override string getTemplateName() => "report-card";
+
+    protected override string updateContent(string content)
+    {
+        content = content.Replace("year.value", DateTime.Today.Year.ToString());
+        content = content.Replace("student.name.value", student.fullName());
+        content = content.Replace("teacher.name.value", teacher.fullName());
+        content = content.Replace("degree.value", degree);
+        content = content.Replace("column.format.value", getColumnQuantity());
+        content = content.Replace("titleLine.value", getTitleLine());
+        content = content.Replace("firstSemester.value", getFirstSemester());
+        content = content.Replace("secondSemester.value", getSecondSemester());
+        content = content.Replace("finalGrade.value", getFinalGrade());
+
+        return content;
+    }
+    
+    private string getColumnQuantity()
+    {
+        var quantity = (subjects.Count + 1).ToString();
+        return $"|*{{{quantity}}}{{c|}}";
+    }
+    private string getTitleLine()
+    {
+        var result = "Parcial";
+
+        foreach (var item in subjects)
+        {
+            result = $"{result} & {item.initials}";
+        }
+
+        return $"{result}\\\\";
+    }
+
+    private string getFirstSemester()
+    {
+        return getSemester(_semesters.First(e => e.semester == 1));
+    }
+
+    private string getSecondSemester()
+    {
+        return getSemester(_semesters.First(e => e.semester == 2));
+    }
+    
+    private string getFinalGrade()
+    {        
+        var finalGrade = "NF";
+
+        var quantity = subjects.Count;
+        
+        while (quantity > 0)
+        {
+            finalGrade = $"{finalGrade} & ";
+            quantity--;
+        }
+
+        return $"{finalGrade}\\\\ \\hline";
+    }
+    
+
+    
+    private string getSemester(SemesterEntity semester)
+    {
+        var firstPartial = student.partials!.First(e => e.partial == 1 && e.semesterId == semester.semesterId);
+        var secondPartial = student.partials!.First(e => e.partial == 2 && e.semesterId == semester.semesterId);
+        
+        var semesterLine = semester.label;
+
+        foreach (var unused in subjects)
+        {
+            var firstGrade = 80;
+            var secondGrade = 70;
+
+            var grade = (firstGrade + secondGrade) / 2;
+            semesterLine = $"{semesterLine} & {grade.ToString()}";
+        }
+
+        semesterLine = $"{semesterLine}\\\\ \\hline";
+        
+        return $"{getGradeByPartial(firstPartial)} {getGradeByPartial(secondPartial)} {semesterLine}";
+    }
+
+    private string getGradeByPartial(PartialEntity partial)
+    {
+        var labelLine = partial.label;
+        var gradeLine = " ";
+
+        foreach (var subject in subjects)
+        {
+            //////////////////////######################################################
+            var result = partial.grades!.FirstOrDefault(e => subject.subjectId == "Hola");
+
+            var label = result == null ? "" : result.label;
+            var grade = result == null ? "" : result.grade.ToString();
+
+            labelLine = $"{labelLine} & {label}";
+            gradeLine = $"{gradeLine} & {grade}";
+        }
+
+        var quantity = (subjects.Count + 1).ToString();
+        labelLine = $"{labelLine}\\\\ \\cline{{2-{quantity}}}\n";
+        gradeLine = $"{gradeLine}\\\\ \\hline \n";
+
+        return $"{labelLine} {gradeLine}";
+    }
+    
+    public class Builder
+    {
+        private readonly GradeReportLatexBuilder latexBuilder;
+
+        public Builder(string templatesPath, string outPath)
+        {
+            latexBuilder = new GradeReportLatexBuilder(templatesPath, outPath);
+        }
+
+        public GradeReportLatexBuilder build() => latexBuilder;
+
+        public Builder withStudent(StudentEntity parameter)
+        {
+            latexBuilder.student = parameter;
+            return this;
+        }
+
+        public Builder withTeacher(TeacherEntity parameter)
+        {
+            latexBuilder.teacher = parameter;
+            return this;
+        }
+        
+        public Builder withDegree(string parameter)
+        {
+            latexBuilder.degree = parameter;
+            return this;
+        }
+        
+        public Builder withSemesters(List<SemesterEntity> parameter)
+        {
+            latexBuilder._semesters = parameter;
+            return this;
+        }
+        
+        public Builder withSubjects(List<(string, string)> parameter)
+        {
+            latexBuilder.subjects = parameter;
+            return this;
+        }
+    }
+}
