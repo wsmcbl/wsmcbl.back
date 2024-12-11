f3dd9b7db5f8b36b3618c6b5fe806a8a534f110c
kenny-tinoco <kenny.tinoco10@gmail.com>
2024-12-10 23:50:39 -0600

    Update: PartialEntity database configuration
    - update SubjectEntity class
    
    Refs: (#78) #80

--- src/model/academy/SubjectPartialEntity.cs
+++ src/model/academy/SubjectPartialEntity.cs
@@ -4,8 +4,8 @@ public class SubjectPartialEntity
 {
     public int subjectPartialId { get; set; }
     public string subjectId { get; set; } = null!;
-    public int partialId { get; set; }
     public string enrollmentId { get; set; } = null!;
+    public int partialId { get; set; }
     public string teacherId { get; set; } = null!;
 
     public ICollection<GradeEntity> gradeList { get; set; } = null!;
