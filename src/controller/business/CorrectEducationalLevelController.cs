2d4c01da1dc02ee87d884415bd7f86f5234fa0d6
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-01-04 11:25:04 -0600

    Feat: add getByStudentId method in CorrectEducationalLevelController class
    - update changeEducationalLevel method
    
    Refs: (#85) #86

--- src/controller/business/CorrectEducationalLevelController.cs
+++ src/controller/business/CorrectEducationalLevelController.cs
@@ -1,10 +1,17 @@
 using wsmcbl.src.exception;
+using wsmcbl.src.model.accounting;
 using wsmcbl.src.model.dao;
 
 namespace wsmcbl.src.controller.business;
 
 public class CorrectEducationalLevelController(DaoFactory daoFactory) : BaseController(daoFactory)
 {
+    public async Task<StudentEntity> getStudentById(string studentId)
+    {
+        var collectTariffController = new CollectTariffController(daoFactory);
+        return await collectTariffController.getStudentById(studentId);
+    }
+    
     public async Task changeEducationalLevel(string studentId, int level)
     {
         if (await daoFactory.academyStudentDao!.hasAEnroll(studentId))
@@ -13,14 +20,18 @@ public class CorrectEducationalLevelController(DaoFactory daoFactory) : BaseCont
         }
         
         var student = await daoFactory.accountingStudentDao!.getById(studentId);
-
         if (student == null)
         {
             throw new EntityNotFoundException("student", studentId);
         }
+
+        var debt = student.getCurrentRegistrationTariffDebt();
+        await daoFactory.debtHistoryDao!.delete(debt);
         
         student.updateEducationalLevel(level);
         await daoFactory.execute();
+
+        await daoFactory.debtHistoryDao.addRegistrationTariffDebtByStudent(student);
     }
 }
 
