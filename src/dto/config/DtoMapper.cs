7d2741d791baadd5a860e3a4998333329bbf7272
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-01-05 21:48:51 -0600

    Feat: add UserToListDto class
    - update DtoMapper and UserToCreateDto classes
    
    Refs: (#88) #92

--- src/dto/config/DtoMapper.cs
+++ src/dto/config/DtoMapper.cs
@@ -7,4 +7,6 @@ public static class DtoMapper
     public static LoginDto mapToDto(this string token) => new(token);
     public static UserToCreateDto mapToCreateDto(this UserEntity user) => new(user);
     public static UserDto mapToDto(this UserEntity user) => new(user);
+    public static List<UserToListDto> mapToListDto(this List<UserEntity> value)
+        => value.Select(e => new UserToListDto(e)).ToList();
 }
