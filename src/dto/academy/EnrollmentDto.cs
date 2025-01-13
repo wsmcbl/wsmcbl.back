d44907f20e7a153710bc06063d2bfe873d402147
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-01-12 21:13:41 -0600

    Feat: add SubjectTeacherIdsDto class
    - update EnrollmentDto class
    
    Refs: (#93) #95

--- src/dto/academy/EnrollmentDto.cs
+++ src/dto/academy/EnrollmentDto.cs
@@ -4,7 +4,45 @@ namespace wsmcbl.src.dto.academy;
 
 public class EnrollmentDto
 {
-    public EnrollmentDto(EnrollmentEntity value)
+    public string enrollmentId { get; set; } = null!;
+    public string? teacherId {get; set; }
+    public string? teacherName { get; set; }
+    public string label { get; set; } = null!;
+    public string? section { get; set; }
+    public int capacity { get; set; }
+    public int quantity { get; set; }
+    
+    public List<SubjectTeacherIdsDto> subjects { get; set; } = null!;
+
+    public EnrollmentDto()
     {
+    }
+    
+    public EnrollmentDto(EnrollmentEntity entity)
+    {
+        enrollmentId = entity.enrollmentId!;
+        teacherId = entity.teacherId;
+        section = entity.section;
+        capacity = entity.capacity;
+        quantity = entity.quantity;
+        label = entity.label;
+
+        if (entity.subjectList == null || entity.subjectList.Count == 0)
+        {
+            subjects = [];
+        }
+
+        subjects = entity.subjectList!.Select(e => new SubjectTeacherIdsDto(e)).ToList();
+    }
+
+    public EnrollmentDto(EnrollmentEntity enrollment, TeacherEntity? teacher) : this(enrollment)
+    {
+        if (teacher == null)
+        {
+            return;
+        }
+        
+        teacherId = teacher.teacherId;
+        teacherName = teacher.fullName();
     }
 }
