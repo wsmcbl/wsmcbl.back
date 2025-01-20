dc4273444f145d0abdb0bd0138657741f61ef569
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-01-19 22:50:36 -0600

    Feat: add AssignPermissionsActions class
    Refs: (#97) #98

---
+++ src/controller/api/AssignPermissionsActions.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using wsmcbl.src.dto.config;
+using wsmcbl.src.middleware;
+
+namespace wsmcbl.src.controller.api;
+
+[ResourceAuthorizer("admin")]
+[Route("config")]
+[ApiController]
+public class AssignPermissionsActions(AssignPermissionsController controller) : ActionsBase
+{
+    /// <summary>Assign permissions and update user.</summary>
+    /// <remarks>
+    /// The secondName and secondSurname can be null or empty.
+    /// The nextCloudGroup can be empty.
+    /// </remarks>
+    /// <response code="201">Returns a user updated.</response>
+    /// <response code="400">If the dto is not valid.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    [HttpPut]
+    [Route("users/{userId}")]
+    public async Task<IActionResult> updateUser(UserToCreateDto dto)
+    {
+        var result = await controller.updateUser(dto.toEntity(), dto.nextCloudGroup);
+        await controller.asignPermissions(dto.permissionList, (Guid)result.userId!);
+        return Ok(new UserToCreateDto(result));
+    }
+}
