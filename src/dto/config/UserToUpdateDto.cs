c1f5e0d9d0f60df96f2510d1c105b09c700531df
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-01-20 18:39:28 -0600

    Feat: add UserToUpdateDto class
    Refs: (#97) #99

---
+++ src/dto/config/UserToUpdateDto.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using wsmcbl.src.model.config;
+
+namespace wsmcbl.src.dto.config;
+
+public class UserToUpdateDto
+{
+    [Required] public string name { get; set; } = null!;
+    public string? secondName { get; set; }
+    [Required] public string surname { get; set; } = null!;
+    public string? secondSurname { get; set; }
+    public List<int> permissionList { get; set; } = null!;
+    public string nextCloudGroup { get; set; } = null!;
+    
+    public UserToUpdateDto(UserEntity user)
+    {
+        name = user.name;
+        secondName = user.secondName;
+        surname = user.surname;
+        secondSurname = user.secondSurname;
+    }
+    
+    public UserEntity toEntity(string userId)
+    {
+        return new UserEntity.Builder((Guid)userId!)
+            .setName(name)
+            .setSecondName(secondName)
+            .setSurname(surname)
+            .setSecondSurname(secondSurname)
+            .build();
+    }
+}
