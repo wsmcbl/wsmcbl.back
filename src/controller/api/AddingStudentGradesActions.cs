24a6cef713b306ad75b526cf5b4db0718bd85d7b
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-01-23 19:25:50 -0600

    Update: getEnrollmentToAddGrades and addGrades endpoints in AddingStudentGradesActions class
    Refs: (#37)

--- src/controller/api/AddingStudentGradesActions.cs
+++ src/controller/api/AddingStudentGradesActions.cs
@@ -3,10 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using wsmcbl.src.controller.business;
 using wsmcbl.src.dto.academy;
 using wsmcbl.src.middleware;
+using wsmcbl.src.model.academy;
 
 namespace wsmcbl.src.controller.api;
 
-[ResourceAuthorizer("admin","teacher")]
+[ResourceAuthorizer("admin", "teacher")]
 [Route("academy")]
 [ApiController]
 public class AddingStudentGradesActions(AddingStudentGradesController controller) : ControllerBase
@@ -22,20 +23,33 @@ public class AddingStudentGradesActions(AddingStudentGradesController controller
         var result = await controller.getPartialList();
         return Ok(result.mapListToDto());
     }
-    
+
     /// <summary>Returns teacher information by id.</summary>
     /// <response code="200">Returns the resource.</response>
     /// <response code="401">If the query was made without authentication.</response>
     /// <response code="403">If the query was made without proper permissions.</response>
     /// <response code="404">Teacher not found.</response>
     [HttpGet]
-    [Route("teacher/{teacherId}")]
+    [Route("teachers/{teacherId}")]
     public async Task<IActionResult> getTeacherById([Required] string teacherId)
     {
         var result = await controller.getTeacherById();
         return Ok(result.mapToBasicDto());
     }
-    
+
+    /// <summary>Returns the list of active enrollment by teacher id.</summary>
+    /// <response code="200">Returns a list, the list can be empty.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    /// <response code="404">Teacher not found.</response>
+    [HttpGet]
+    [Route("teachers/{teacherId}/enrollments")]
+    public async Task<IActionResult> getEnrollmentListByTeacherId([Required] string teacherId)
+    {
+        var result = await controller.getEnrollmentListByTeacherId(teacherId);
+        return Ok(result.mapListToDto());
+    }
+
     /// <summary>
     ///  Returns the list of subject, students and grades corresponding to a teacher grade's information
     /// </summary>
@@ -44,28 +58,18 @@ public class AddingStudentGradesActions(AddingStudentGradesController controller
     /// <response code="403">If the query was made without proper permissions.</response>
     /// <response code="404">Teacher, enrollment or partial not found.</response>
     [HttpGet]
-    [Route("enrollments")]
-    public async Task<IActionResult> getEnrollmentToAddGrades(TeacherEnrollmentDto dto)
+    [Route("teachers/{teacherId}/enrollments/{enrollmentId}")]
+    public async Task<IActionResult> getEnrollmentToAddGrades([Required] string teacherId,
+        [Required] string enrollmentId, [Required] [FromQuery] int partial)
     {
-        var enrollment = await controller.getEnrollmentById(dto.enrollmentId);
-        var subjectPartialList = await controller.getSubjectPartialList(dto.toEntity());
+        var enrollment = await controller.getEnrollmentById(enrollmentId);
+
+        var subjectPartial = new SubjectPartialEntity(teacherId, enrollmentId, partial);
+        var subjectPartialList = await controller.getSubjectPartialList(subjectPartial);
 
         return Ok(new EnrollmentToAddGradesDto(enrollment, subjectPartialList));
     }
-    
-    /// <summary>Returns the list of active enrollment by teacher id.</summary>
-    /// <response code="200">Returns a list, the list can be empty.</response>
-    /// <response code="401">If the query was made without authentication.</response>
-    /// <response code="403">If the query was made without proper permissions.</response>
-    /// <response code="404">Teacher not found.</response>
-    [HttpGet]
-    [Route("enrollments/{teacherId}")]
-    public async Task<IActionResult> getEnrollmentListByTeacherId([Required] string teacherId)
-    {
-        var result = await controller.getEnrollmentListByTeacherId(teacherId);
-        return Ok(result.mapListToDto());
-    }
-    
+
     /// <summary>
     ///  Update the grades of subject corresponding to a teacher grade's information
     /// </summary>
@@ -75,10 +79,12 @@ public class AddingStudentGradesActions(AddingStudentGradesController controller
     /// <response code="403">If the query was made without proper permissions.</response>
     /// <response code="404">Teacher or internal record not found.</response>
     [HttpPut]
-    [Route("enrollments/subjects/grades")]
-    public async Task<IActionResult> addGrades(GradesToAddDto dto)
+    [Route("teachers/{teacherId}/enrollments/{enrollmentId}")]
+    public async Task<IActionResult> addGrades([Required] string teacherId,
+        [Required] string enrollmentId, [Required] [FromQuery] int partial, List<GradeDto> gradeList)
     {
-        await controller.addGrades(dto.getSubjectPartial(), dto.getGradeList());
+        var subjectPartial = new SubjectPartialEntity(teacherId, enrollmentId, partial);
+        await controller.addGrades(subjectPartial, gradeList.toEntity());
         return Ok();
     }
 }
