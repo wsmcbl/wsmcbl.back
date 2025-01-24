e15e2196eb3610dfdb7558a80ac0dcbe89e5cfa6
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-01-23 20:56:08 -0600

    Feat: add TeacherDto class
    Refs: (#37)

--- src/dto/academy/TeacherDto.cs
+++ src/dto/academy/TeacherDto.cs
@@ -2,16 +2,20 @@ using wsmcbl.src.model.academy;
 
 namespace wsmcbl.src.dto.academy;
 
-public class BasicTeacherDto
+public class TeacherDto
 {
     public string teacherId { get; set; }
     public string fullName { get; set; }
     public bool isGuide { get; set; }
+    public string enrollmentId { get; set; }
+    public string enrollmentLabel { get; set; }
 
-    public BasicTeacherDto(TeacherEntity entity)
+    public TeacherDto(TeacherEntity entity)
     {
         teacherId = entity.teacherId!;
         fullName = entity.fullName();
         isGuide = entity.isGuide;
+        enrollmentId = entity.enrollment!.enrollmentId!;
+        enrollmentLabel = entity.enrollment!.label;
     }
 }
