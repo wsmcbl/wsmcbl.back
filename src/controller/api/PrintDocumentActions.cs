f78488b68338ab1f3591d8fec885a94a56da4ff4
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-01-23 23:17:49 -0600

    Feat: add PrintDocumentActions class
    Refs: (#104)

---
+++ src/controller/api/PrintDocumentActions.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using wsmcbl.src.middleware;
+
+namespace wsmcbl.src.controller.api;
+
+[ResourceAuthorizer("admin", "secretary")]
+[Route("secretary")]
+[ApiController]
+public class PrintDocumentActions(PrintDocumentController controller) : ActionsBase
+{
+    /// <summary>
+    ///  Returns the assistance document of all degrees.
+    /// </summary>
+    /// <response code="200">Return existing resources.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    /// <response code="404">Resource depends on another resource not found.</response>
+    [HttpGet]
+    [Route("degrees/documents")]
+    public async Task<IActionResult> getAssistanceDocument()
+    {
+        var result = await controller.getAssistanceListDocument();
+        return File(result, "application/pdf", "assistance-list.pdf");
+    }    
+}
