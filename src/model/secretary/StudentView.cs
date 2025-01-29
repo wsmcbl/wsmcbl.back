99ec3d7d40ee4eec1c22afe59d3083b37e2a3b37
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-01-29 14:05:59 -0600

    Update: add initLabels method in StudentView class

--- src/model/secretary/StudentView.cs
+++ src/model/secretary/StudentView.cs
@@ -5,6 +5,12 @@ public class StudentView
     public string studentId { get; set; } = null!;
     public string fullName { get; set; } = null!;
     public bool isActive { get; set; }
-    public string schoolyear { get; set; } = null!;
-    public string enrollment { get; set; } = null!;
+    public string? schoolyear { get; set; }
+    public string? enrollment { get; set; }
+
+    public void initLabels()
+    {
+        schoolyear ??= string.Empty;
+        enrollment ??= string.Empty;
+    }
 }
