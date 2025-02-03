eb10bb9c71e6efa6fb1c01758fc9d05f3255e787
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-02-02 18:44:38 -0600

    Feat: add getRolesList method in ViewUserProfileActions class
    Refs: (#105)

--- src/controller/api/ViewUserProfileActions.cs
+++ src/controller/api/ViewUserProfileActions.cs
@@ -6,7 +6,7 @@ using wsmcbl.src.middleware;
 
 namespace wsmcbl.src.controller.api;
 
-[Route("config/users")]
+[Route("config")]
 [ApiController]
 public class ViewUserProfileActions(ViewUserProfileController controller) : ActionsBase
 {
@@ -17,12 +17,24 @@ public class ViewUserProfileActions(ViewUserProfileController controller) : Acti
     /// <response code="404">If the user not exist.</response>
     [ResourceAuthorizer("user:read")]
     [HttpGet]
-    [Route("{userId}")]
+    [Route("users/{userId}")]
     public async Task<IActionResult> getUser([Required] string userId)
     {
         var result = await controller.getUserById(userId);
         var nextCloudGroup = await controller.getNextCloudGroupByUser(result);
         
         return CreatedAtAction(null, result.mapToDto(nextCloudGroup));
+    }
+    
+    /// <summary>Get roles list.</summary>
+    /// <response code="200">Returns a list information, the list can be empty.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    [ResourceAuthorizer("rol:read")]
+    [HttpGet]
+    [Route("roles")]
+    public async Task<IActionResult> getRolesList([Required] string userId)
+    {
+        return Ok(await controller.getRolesList());
     }
 }
