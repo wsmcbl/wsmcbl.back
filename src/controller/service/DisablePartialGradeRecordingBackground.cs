cd2ad093b40d12b34ca8ef1ce129983de7b617ec
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-02-03 20:40:48 -0600

    Feat: add DisablePartialGradeRecordingBackground class
    Refs: (#106) #107

---
+++ src/controller/service/DisablePartialGradeRecordingBackground.cs
@@ -0,0 +1,49 @@
+using wsmcbl.src.model.academy;
+using wsmcbl.src.model.dao;
+
+namespace wsmcbl.src.controller.service;
+
+public class DisablePartialGradeRecordingBackground : BackgroundService
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+
+    public DisablePartialGradeRecordingBackground(IServiceScopeFactory scopeFactory)
+    {
+        _scopeFactory = scopeFactory;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await checkPartials();
+            await Task.Delay(TimeSpan.FromHours(1), stoppingToken);
+        }
+    }
+
+    private async Task checkPartials()
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var daoFactory = scope.ServiceProvider.GetRequiredService<DaoFactory>();
+
+        var partialList = await daoFactory.partialDao!.getListInCurrentSchoolyear();
+        
+        var item = partialList.FirstOrDefault(e => e.gradeRecordIsActive);
+        if (item == null)
+        {
+            return;
+        }
+
+        if (DateTime.Now > item.gradeRecordDeadline)
+        {
+            item.disableGradeRecording();
+            await daoFactory.execute();
+            await sendNotification(item);
+        }
+    }
+
+    private async Task sendNotification(PartialEntity partial)
+    {
+        await Task.CompletedTask;
+    }
+}
