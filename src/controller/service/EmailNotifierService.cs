3d68f5e220aa1c45a759ee9715cfd10e2117db95
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-02-06 09:48:19 -0600

    Update: EmailNotifierService class
    Refs: (#106) #107

--- src/controller/service/EmailNotifierService.cs
+++ src/controller/service/EmailNotifierService.cs
@@ -1,5 +1,6 @@
 using MailKit.Net.Smtp;
 using MimeKit;
+using wsmcbl.src.exception;
 
 namespace wsmcbl.src.controller.service;
 
@@ -9,29 +10,35 @@ public class EmailNotifierService
     private const int _smtpPort = 587;
     private const string _smtpUser = "info@cbl-edu.com";
 
-    public async Task sendEmail(string email, string subject, string mensaje)
+    public async Task sendEmail(List<string> emailList, string subject, string message)
     {
         try
         {
-            var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress(getTitle(), _smtpUser));
-            emailMessage.To.Add(new MailboxAddress("", email));
-            emailMessage.Subject = subject;
-
-            emailMessage.Body = new TextPart("html")
-            {
-                Text = mensaje
-            };
-
             using (var client = new SmtpClient())
             {
                 await client.ConnectAsync(_smtpServer, _smtpPort, MailKit.Security.SecureSocketOptions.StartTls); 
-                await client.AuthenticateAsync(_smtpUser, getPass());
-                await client.SendAsync(emailMessage);
+                await client.AuthenticateAsync(_smtpUser, getPassword());
+                
+                foreach (var email in emailList)
+                {
+                    var emailMessage = new MimeMessage();
+                    emailMessage.From.Add(new MailboxAddress(getTitle(), _smtpUser));
+                    emailMessage.To.Add(new MailboxAddress("", email));
+                    emailMessage.Subject = subject;
+
+                    emailMessage.Body = new TextPart("html")
+                    {
+                        Text = message
+                    };
+                    
+                    await client.SendAsync(emailMessage);    
+                }
+                
                 await client.DisconnectAsync(true);
             }
 
-            Console.WriteLine($"Mail sent to {email}");
+            var d = emailList.Aggregate("", (current, email) => current + $", {email}");
+            Console.WriteLine($"Mail sent to {d}");
         }
         catch (Exception ex)
         {
@@ -39,10 +46,16 @@ public class EmailNotifierService
         }
     }
 
-    private string getTitle() => "Registro de calificaciones";
+    private static string getTitle() => "Registro de calificaciones";
 
-    private string getPass()
+    private static string getPassword()
     {
-        return "temporal";
+        var value = Environment.GetEnvironmentVariable("INFO_EMAIL_PASSWORD");
+        if (value == null)
+        {
+            throw new InternalException("INFO_EMAIL_PASSWORD environment not found.");
+        }
+
+        return value;
     }
 }
