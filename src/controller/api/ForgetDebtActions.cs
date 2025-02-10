548ddff07dc18d488ba4ed144ff3faccf1fb7c89
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-02-10 15:43:01 -0600

    Update: authorizer permission in ForgetDebtActions class
    Refs: (#97) #98

--- src/controller/api/ForgetDebtActions.cs
+++ src/controller/api/ForgetDebtActions.cs
@@ -6,35 +6,32 @@ using wsmcbl.src.middleware;
 
 namespace wsmcbl.src.controller.api;
 
-[ResourceAuthorizer("admin", "cashier")]
-[Route("accounting")]
+[Route("accounting/debts")]
 [ApiController]
 public class ForgetDebtActions(ForgetDebtController controller) : ActionsBase
 {
-    /// <summary>
-    ///  Returns the list of debts by student id
-    /// </summary>
+    /// <summary>Returns the list of debts by student id.</summary>
     /// <response code="200">Returns a list, the list can be empty.</response>
     /// <response code="401">If the query was made without authentication.</response>
     /// <response code="403">If the query was made without proper permissions.</response>
     [HttpGet]
-    [Route("debts")]
+    [Route("")]
+    [ResourceAuthorizer("debt:read")]
     public async Task<IActionResult> getDebtListByStudent([FromQuery] string studentId)
     {
         var result = await controller.getDebtListByStudent(studentId);
         return Ok(result.mapToListDto());
     }
     
-    /// <summary>
-    ///  Update forgive a debt.
-    /// </summary>
+    /// <summary>Update forgive a debt.</summary>
     /// <response code="200">Returns the edited resource.</response>
     /// <response code="401">If the query was made without authentication.</response>
     /// <response code="403">If the query was made without proper permissions.</response>
     /// <response code="404">If resource not exist(student or tariff).</response>
     /// <response code="409">If the debt is already paid.</response>
     [HttpPut]
-    [Route("debts")]
+    [Route("")]
+    [ResourceAuthorizer("debt:update")]
     public async Task<IActionResult> forgiveADebt(ForgetDebtDto dto)
     {
         if (!dto.authorizationToken.Equals("36987"))
