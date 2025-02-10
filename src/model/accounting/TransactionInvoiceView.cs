6dc357e4543932d1a68a27029bddf65d5b1acfac
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-02-09 23:10:45 -0600

    Update: add TransactionInvoiceView context configuration

--- src/model/accounting/TransactionInvoiceView.cs
+++ src/model/accounting/TransactionInvoiceView.cs
@@ -5,7 +5,7 @@ public class TransactionInvoiceView
     public string transactionId { get; set; } = null!;
     public int number { get; set; }
     public string studentId { get; set; } = null!;
-    public string studentName { get; set; } = null!;
+    public string student { get; set; } = null!;
     public double total { get; set; }
     public bool isValid { get; set; }
     public string concept { get; set; } = null!;
