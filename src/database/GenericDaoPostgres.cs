40d240bc7ad2cc5e2610ed4caa898c2a5fd3b998
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-02-16 21:52:47 -0600

    Feat: add getPaged method in GenericDaoPostgres class
    Refs: (#115)

--- src/database/GenericDaoPostgres.cs
+++ src/database/GenericDaoPostgres.cs
@@ -53,4 +53,29 @@ public abstract class GenericDaoPostgres<T, ID> : IGenericDao<T, ID> where T : c
             throw new ForbiddenException("Failed to perform transaction. Error: " + e.Message);
         }
     }
+
+    public async Task<PagedResult<T>> getPaged(PagedQuery request)
+    {
+        var query = context.Set<T>().AsQueryable();
+        var totalCount = await query.CountAsync();
+
+        if (!string.IsNullOrEmpty(request.sortBy))
+        {
+            query = request.isDescending ? query.OrderByDescending(e => EF.Property<object>(e, request.sortBy))
+                : query.OrderBy(e => EF.Property<object>(e, request.sortBy));
+        }
+
+        var data = await query
+            .Skip((request.page - 1) * request.pageSize)
+            .Take(request.pageSize)
+            .ToListAsync();
+
+        return new PagedResult<T>
+        {
+            data = data,
+            quantity = totalCount,
+            page = request.page,
+            pageSize = request.pageSize
+        };
+    }
 }
