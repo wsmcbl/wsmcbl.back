87f0a3e9114e56bfd591b0093a8a1f1bfdc5d938
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-02-17 09:05:40 -0600

    Feat: add setup method in PageResult class
    Refs: (#115)

--- src/model/dao/PagedResult.cs
+++ src/model/dao/PagedResult.cs
@@ -6,5 +6,21 @@ public class PagedResult<T>
     public int page { get; set; }
     public int pageSize { get; set; }
     public int quantity { get; set; }
-    public int totalPages => (int)Math.Ceiling((double)quantity / pageSize);    
+    public int totalPages => (int)Math.Ceiling((double)quantity / pageSize);
+
+    public PagedResult()
+    {
+    }
+
+    public PagedResult(List<T> list)
+    {
+        data = list;
+    }
+
+    public void setup<P>(PagedResult<P> parameter) where P : class
+    {
+        page = parameter.page;
+        pageSize = parameter.pageSize;
+        quantity = parameter.quantity;
+    }
 }
