cb4f2f717b63d9abb1014fb0a480896ecfb97611
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-02-17 10:42:17 -0600

    Feat: add PageRequest class
    Refs: (#115)

--- src/model/dao/PagedRequest.cs
+++ src/model/dao/PagedRequest.cs
@@ -4,7 +4,7 @@ public class PagedRequest
 {
     public string? search { get; set; }
     public string? sortBy { get; set; }
-    public bool isDescending { get; set; } = false;
+    public bool isDescending { get; set; } = true;
     public int page { get; set; } = 1;
     public int pageSize { get; set; } = 10;
 }
