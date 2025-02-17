baca767bd2138b4a65d198971032bafab7eccd68
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-02-17 13:50:19 -0600

    Feat: add GenericDaoWithPagedPostgres class
    Refs: (#115)

--- src/model/dao/IGenericDaoWithPaged.cs
+++ src/model/dao/IGenericDaoWithPaged.cs
@@ -3,6 +3,6 @@ namespace wsmcbl.src.model.dao;
 public interface IGenericDaoWithPaged<T, in ID> : IGenericDao<T, ID>
 {
     public Task<PagedResult<T>> getPaged(PagedRequest request);
-    public Task<IQueryable<T>> search(IQueryable<T> query, PagedRequest request);
-    public Task<IQueryable<T>> filter(IQueryable<T> query, PagedRequest request);
+    public IQueryable<T> search(IQueryable<T> query, PagedRequest request);
+    public IQueryable<T> filter(IQueryable<T> query, PagedRequest request);
 }
