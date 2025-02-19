6457190ebff6b9df635487ee44cb7a67e232ec80
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-02-19 13:42:50 -0600

    Feat: add search method in AccountingStudentDaoPostgres class
    Refs: (#115)

--- src/database/AccountingStudentDaoPostgres.cs
+++ src/database/AccountingStudentDaoPostgres.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using wsmcbl.src.database.context;
+using wsmcbl.src.database.service;
 using wsmcbl.src.exception;
 using wsmcbl.src.model;
 using wsmcbl.src.model.accounting;
@@ -49,11 +50,25 @@ public class AccountingStudentDaoPostgres : GenericDaoPostgres<StudentEntity, st
         return result.enrollment!;
     }
     
-
-    public async Task<List<StudentView>> getStudentViewList()
+    public async Task<PagedResult<StudentView>> getStudentViewList(PagedRequest request)
     {
-        return await context.Set<StudentView>().AsNoTracking()
-            .Where(e => e.isActive).ToListAsync();
+        var query = context.GetQueryable<StudentView>().Where(e => e.isActive);
+
+        var pagedService = new PagedService<StudentView>(query, search);
+
+        request.setDefaultSort("fullName");
+        return await pagedService.getPaged(request);
+    }
+    
+    private IQueryable<StudentView> search(IQueryable<StudentView> query, string search)
+    { 
+        var value = $"%{search}%";
+        
+        return query.Where(e =>
+            EF.Functions.Like(e.studentId, value) ||
+            EF.Functions.Like(e.fullName.ToLower(), value) ||
+            EF.Functions.Like(e.tutor.ToLower(), value) ||
+            (e.enrollment != null && EF.Functions.Like(e.enrollment.ToLower(), value)));
     }
 
     public async Task<List<StudentEntity>> getAllWithSolvencyInRegistration()
