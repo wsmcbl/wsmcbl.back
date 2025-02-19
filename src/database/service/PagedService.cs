9bc95f9572094f5b10a7d7909cd426e51da1ed7f
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-02-18 23:27:38 -0600

    Feat: add PagedService class
    Refs: (#115)

---
+++ src/database/service/PagedService.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using wsmcbl.src.model.dao;
+
+namespace wsmcbl.src.database.service;
+
+public class PagedService<T> where T : class
+{
+    private IQueryable<T> query { get; set; }
+    
+    public PagedService(IQueryable<T> query)
+    {
+        this.query = query; 
+    }
+    
+    public async Task<PagedResult<T>> getPaged(PagedRequest request)
+    {
+        if (!string.IsNullOrWhiteSpace(request.search))
+        {
+            query = search(request);
+        }
+
+        query = sort(request);
+
+        var totalCount = await query.CountAsync();
+
+        var data = await query
+            .Skip((request.page - 1) * request.pageSize)
+            .Take(request.pageSize)
+            .ToListAsync();
+
+        return new PagedResult<T>
+        {
+            data = data,
+            quantity = totalCount,
+            page = request.page,
+            pageSize = request.pageSize
+        };
+    }
+
+    public IQueryable<T> search(PagedRequest request)
+    {
+        return query;
+    }
+
+    public IQueryable<T> sort(PagedRequest request)
+    {
+        return query;
+    }
+}
