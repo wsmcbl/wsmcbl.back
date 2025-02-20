80592ccf4fb4bbbee8bb627476c1d4783602e64d
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-02-20 14:04:28 -0600

    Update: delete parseToDateTime method in TransactionReportByDateActions class
    Refs: (#115)

--- src/controller/api/TransactionReportByDateActions.cs
+++ src/controller/api/TransactionReportByDateActions.cs
@@ -15,14 +15,13 @@ public class TransactionReportByDateActions(TransactionReportByDateController co
 {
     /// <summary>Returns summary list of transactions and revenues by date.</summary>
     /// <remarks> The date values must be "day-month-year" format, example "25-01-2025".</remarks>
-    /// <remarks> A date before 2,000 is not accepted.</remarks>
-    /// <param name="request">Paged request</param>
-    /// <param name="request.from">The default time is set to 00:00 hours.</param>
-    /// <param name="request.to">
+    /// <remarks> A date before 2,000 is not accepted.
+    /// The default time is set to 00:00 hours.
     /// The default time is set to 23:59.
     /// If the date entered corresponds to the current date,
     /// the time will be adjusted to the time at which the query is made.
-    /// </param>
+    /// </remarks>
+    /// <param name="request">Paged request</param>
     /// <response code="200">Returns a list, the list can be empty.</response>
     /// <response code="400">If any of the dates are not valid.</response>
     /// <response code="401">If the query was made without authentication.</response>
@@ -37,18 +36,19 @@ public class TransactionReportByDateActions(TransactionReportByDateController co
             throw new IncorrectDataBadRequestException("Some of the dates are not in the correct format.");
         }
         
+        request.parseRange();
+        
         var result = await controller.getTransactionList(request);
         
         var pagedResult = new PagedReportByDateDto(result.data.mapToListDto());
         pagedResult.setup(result);
         
-        var dates = parseToDateTime(request.from!, request.to!);
-        var summaryReport = await controller.getSummary(dates.from, dates.to);
+        var summaryReport = await controller.getSummary(request.From(), request.To());
         
         pagedResult.setValidTransactionData(summaryReport[0]);
         pagedResult.setInvalidTransactionData(summaryReport[1]);
         
-        pagedResult.setDateRange(dates.from, dates.to);
+        pagedResult.setDateRange(request.From(), request.To());
         pagedResult.setUserName(await controller.getUserName(getAuthenticatedUserId()));
         
         return Ok(pagedResult);
@@ -74,27 +74,6 @@ public class TransactionReportByDateActions(TransactionReportByDateController co
             return false;
         }
     }
-
-    public static (DateTime from, DateTime to) parseToDateTime(string from, string to)
-    {
-        var startDate = DateTime.ParseExact(from, "dd-MM-yyyy", CultureInfo.InvariantCulture);
-        var endDate = DateTime.ParseExact(to, "dd-MM-yyyy", CultureInfo.InvariantCulture);
-
-        if (startDate.Date > endDate.Date)
-        {
-            throw new BadRequestException("The date range is not valid.");
-        }
-
-        startDate = startDate.setHours(6);
-        
-        endDate = isToday(endDate) ? DateTime.UtcNow :
-            endDate.setHours(0).Date.AddDays(1).AddHours(6).AddSeconds(-1);
-        
-        return (startDate, endDate);
-    }
-
-    private static bool isToday(DateTime date) => date.Date == DateTime.Today.Date;
-    
 
     /// <summary>Returns the list of tariff type.</summary>
     /// <response code="200">Return existing resources (can be empty list).</response>
