151c2814713fd975aeffb48443f2bf233a845519
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-03 14:05:56 -0600

    Feat: add getRoleById method in UpdateRolesActions class
    Refs: (#123) #124

--- src/controller/api/UpdateRolesActions.cs
+++ src/controller/api/UpdateRolesActions.cs
@@ -18,5 +18,18 @@ public class UpdateRolesActions(UpdateRolesController controller) : ActionsBase
     public async Task<IActionResult> getRoleList()
     {
         return Ok(await controller.getRoleList());
+    }   
+    
+    /// <summary>Get roles by id.</summary>
+    /// <response code="201">Returns a role updated.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    /// <response code="404">If the role not exist.</response>
+    [HttpGet]
+    [Route("roles/{roleId:int}")]
+    [ResourceAuthorizer("rol:read")]
+    public async Task<IActionResult> getRoleById(int roleId)
+    {
+        return Ok(await controller.getRoleById(roleId));
     }    
 }
