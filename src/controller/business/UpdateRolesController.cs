e5f3953daf47091624f685016df63fdc08b68688
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-03 13:55:29 -0600

    Feat: add getRoleList method in UpdateRolesController class
    Refs: (#123) #124

--- src/controller/business/UpdateRolesController.cs
+++ src/controller/business/UpdateRolesController.cs
@@ -1,8 +1,12 @@
+using wsmcbl.src.model.config;
 using wsmcbl.src.model.dao;
 
 namespace wsmcbl.src.controller.business;
 
 public class UpdateRolesController(DaoFactory daoFactory) : BaseController(daoFactory)
 {
-    
+    public async Task<List<RoleEntity>> getRoleList()
+    {
+        return await daoFactory.roleDao!.getAll();
+    }
 }
