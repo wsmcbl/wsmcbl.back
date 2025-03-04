ffbd85ee6cadf428963569df10d9a4af8860ead3
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-04 17:30:51 -0600

    Feat: add equals method in UserPermissionEntity and RolePermissionEntity classes
    Refs: (#97)

--- src/model/config/UserPermissionEntity.cs
+++ src/model/config/UserPermissionEntity.cs
@@ -6,4 +6,9 @@ public class UserPermissionEntity
     public int permissionId { get; set; }
 
     public PermissionEntity? permission { get; set; }
+
+    public bool equals(UserPermissionEntity value)
+    {
+        return userId == value.userId && permissionId == value.permissionId;
+    }
 }
