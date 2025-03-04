f69da9a0de031f0d779de87e584aa7733ee38af2
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-03 20:09:05 -0600

    Feat: add rolePermissionList property in RoleEntity class
    Refs: (#123) #126

--- src/model/config/RoleEntity.cs
+++ src/model/config/RoleEntity.cs
@@ -5,7 +5,8 @@ public class RoleEntity
     public int roleId { get; set; }
     public string name { get; set; } = null!;
     public string description { get; set; } = null!;
-    
+
+    public List<RolePermissionEntity> rolePermissionList { get; set; } = [];
     public List<PermissionEntity> permissionList { get; set; } = [];
 
     public void updateRolePermissionList(List<PermissionEntity> list, IRolePermissionDao rolePermissionDao)
