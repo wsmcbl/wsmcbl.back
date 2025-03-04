1218973b3a317448c2c70f12c690dd6f6ab7e1b5
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-03 19:47:35 -0600

    Feat: add constructor in PermissionEntity class
    Refs: (#123) #126

--- src/model/config/PermissionEntity.cs
+++ src/model/config/PermissionEntity.cs
@@ -3,8 +3,17 @@ namespace wsmcbl.src.model.config;
 public class PermissionEntity
 {
     public int permissionId { get; set; }
-    public string area { get; set; } = null!;
-    public string name { get; set; } = null!;
-    public string spanishName { get; set; } = null!;
-    public string description { get; set; } = null!;
+    public string area { get; set; }
+    public string name { get; set; }
+    public string spanishName { get; set; }
+    public string description { get; set; }
+
+    public PermissionEntity(int id)
+    {
+        permissionId = id;
+        area = string.Empty;
+        name = string.Empty;
+        spanishName = string.Empty;
+        description = string.Empty;
+    }
 }
