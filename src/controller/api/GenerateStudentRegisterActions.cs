c5a3ee09691cafa8a6527a686f21f6be19fe2a3b
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-04 19:16:34 -0600

    Feat: add GenerateStudentRegisterActions class
    Refs: (#128) #129

---
+++ src/controller/api/GenerateStudentRegisterActions.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using wsmcbl.src.controller.business;
+using wsmcbl.src.middleware;
+using wsmcbl.src.model;
+using wsmcbl.src.model.secretary;
+
+namespace wsmcbl.src.controller.api;
+
+[Route("secretary/students/registers")]
+[ApiController]
+public class GenerateStudentRegisterActions(GenerateStudentRegisterController controller) : ActionsBase
+{
+    /// <summary>Returns paged student register.</summary>
+    /// <remarks>Values for sortBy: studentId, fullName, isActive, tutor, schoolyear and enrollment.</remarks>
+    /// <response code="200">Returns a resource.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    [HttpGet]
+    [Route("")]
+    [ResourceAuthorizer("student:read")]
+    public async Task<IActionResult> getStudentRegisterList([FromQuery] StudentPagedRequest request)
+    {
+        request.checkSortByValue(["studentId", "fullName", "isActive", "tutor", "schoolyear", "enrollment"]);
+        
+        var result = await controller.getStudentRegisterList(request);
+        
+        var pagedResult = new PagedResult<StudentEntity>(result.data.mapToListBasicDto());
+        pagedResult.setup(result);
+        
+        return Ok(pagedResult);
+    }
+}
