3f46959d2e17f39b662f774c1fa44f365e321fa8
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-07 12:37:47 -0600

    Feat: add setTitle, setBody and setHeader methods in SpreadSheetMaker class
    Refs: (#128) #129

--- src/controller/service/SpreadSheetMaker.cs
+++ src/controller/service/SpreadSheetMaker.cs
@@ -1,5 +1,6 @@
 using ClosedXML.Excel;
 using wsmcbl.src.model.dao;
+using wsmcbl.src.model.secretary;
 using wsmcbl.src.utilities;
 
 namespace wsmcbl.src.controller.service;
@@ -7,6 +8,9 @@ namespace wsmcbl.src.controller.service;
 public class SpreadSheetMaker
 {
     private DaoFactory daoFactory {get; set;}
+
+    private const string lastColumnName = "V";
+    private IXLWorksheet? worksheet { get; set; }
     
     public SpreadSheetMaker(DaoFactory daoFactory)
     {
@@ -18,77 +22,136 @@ public class SpreadSheetMaker
         var user = await daoFactory.userDao!.getById(userId);
         var currentSchoolyear = await daoFactory.schoolyearDao!.getCurrentOrNew();
         var list = await daoFactory.studentDao!.getStudentRegisterInCurrentSchoolyear();
+
+        var title = $"Padrón {currentSchoolyear.label}";
         
         using var workbook = new XLWorkbook();
-        var worksheet = workbook.Worksheets.Add($"Padrón {currentSchoolyear.label}");
+        worksheet = workbook.Worksheets.Add(title);
+        worksheet.Style.Font.FontSize = 12;
         
         worksheet.CellsUsed().Style.NumberFormat.SetFormat("@");
         
-        // Aplicar estilo a los encabezados
-        var headerStyle = worksheet.Range("B1:T1");
-        headerStyle.Style.Font.Bold = true;
-        headerStyle.Style.Fill.BackgroundColor = XLColor.LightGray;
-        headerStyle.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-        headerStyle.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
-
-        worksheet.Cell(1, 2).Value = user.getAlias();
+        setTitle(title);
+        setDate(5, user.getAlias());
+        const int headerRow = 7;
+        setHeader(headerRow);
         
-        var header = 2;
-        worksheet.Cell(header, 2).Value = "Código";
-        worksheet.Cell(header, 3).Value = "Cód. Mined";
-        worksheet.Cell(header, 4).Value = "Nombre";
-        worksheet.Cell(header, 5).Value = "Modalidad";
-        worksheet.Cell(header, 6).Value = "Grado";
-        worksheet.Cell(header, 7).Value = "Seción";
-        worksheet.Cell(header, 8).Value = "F. Matrícula";
-        worksheet.Cell(header, 9).Value = "Sexo";
-        worksheet.Cell(header, 10).Value = "F. Nacimiento";
-        worksheet.Cell(header, 11).Value = "Edad";
-        worksheet.Cell(header, 12).Value = "Talla";
-        worksheet.Cell(header, 13).Value = "Peso";
-        worksheet.Cell(header, 14).Value = "Tutor";
-        worksheet.Cell(header, 15).Value = "Telefono";
-        worksheet.Cell(header, 16).Value = "Padre";
-        worksheet.Cell(header, 17).Value = "C. padre";
-        worksheet.Cell(header, 18).Value = "Madre";
-        worksheet.Cell(header, 19).Value = "C. Madre";
-        worksheet.Cell(header, 20).Value = "Repitente";
-
-        var counter = 3;
+        var counter = headerRow + 1;
         foreach (var item in list.OrderBy(e => e.degreePosition))
         {
-            worksheet.Cell(counter, 2).Value = item.studentId; 
-            worksheet.Cell(counter, 3).Value = item.minedId.getOrDefault(); 
-            worksheet.Cell(counter, 4).Value = item.fullName; 
-            worksheet.Cell(counter, 5).Value = item.educationalLevel; 
-            worksheet.Cell(counter, 6).Value = item.degree; 
-            worksheet.Cell(counter, 7).Value = item.section; 
-            worksheet.Cell(counter, 8).Value = item.enrollDate; 
-            worksheet.Cell(counter, 9).Value = item.sex;
-            worksheet.Cell(counter, 10).Value = item.birthday.ToString("dd-MM-yyyy"); 
-            worksheet.Cell(counter, 11).Value = item.getAge(); 
-            worksheet.Cell(counter, 12).Value = item.height; 
-            worksheet.Cell(counter, 13).Value = item.weight; 
-            worksheet.Cell(counter, 14).Value = item.tutor; 
-            worksheet.Cell(counter, 15).Value = item.phone; 
-            worksheet.Cell(counter, 16).Value = item.father; 
-            worksheet.Cell(counter, 17).Value = item.fatherIdCard; 
-            worksheet.Cell(counter, 18).Value = item.mother; 
-            worksheet.Cell(counter, 19).Value = item.motherIdCard; 
-            worksheet.Cell(counter, 20).Value = item.isRepeating.ToString() ?? "d";
-
-            var rowStyle = worksheet.Range($"B{counter}:T{counter}");
-            rowStyle.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+            setBody(counter, item, counter - headerRow);
+            var rowStyle = worksheet.Range($"B{counter}:{lastColumnName}{counter}");
             counter++;
         }
 
-        worksheet.Columns().AdjustToContents(); // Ajustar ancho de columnas
-        worksheet.SheetView.FreezeRows(1); // Fijar encabezado
+        var lastRow = list.Count + headerRow;
+        
+        setBorder(lastRow, headerRow);
+        
+        worksheet.Columns().AdjustToContents();
+        worksheet.SheetView.FreezeRows(headerRow);
 
         using var stream = new MemoryStream();
         workbook.SaveAs(stream);
         stream.Position = 0;
 
         return stream.ToArray();
+    }
+
+    private void setDate(int row, string alias)
+    {
+        var dateCell = worksheet!.Range($"B{row}:{lastColumnName}{row}").Merge();
+        dateCell.Value = $"Generado por wsmcbl el {DateTime.UtcNow.toStringUtc6()}, {alias}.";
+        dateCell.Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+    }
+
+    private void setBorder(int lastRow, int headerRow)
+    {
+        var tableRange = worksheet!.Range($"B{headerRow}:{lastColumnName}{lastRow}");
+
+        tableRange.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+        tableRange.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+        tableRange.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+        tableRange.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+        tableRange.Style.Border.InsideBorder = XLBorderStyleValues.Thin;
+    }
+
+    private void setTitle(string title)
+    { 
+        var titleRow = 2;
+        var titleCell = worksheet!.Range($"B{titleRow}:{lastColumnName}{titleRow}").Merge();
+        titleCell.Value = "Colegio Bautista Libertad";
+        titleCell.Style.Font.Bold = true;
+        titleCell.Style.Font.FontSize = 14;
+        titleCell.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+        titleCell.Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+        worksheet.Row(titleRow).Height = 25;
+        
+        
+        var titleCell1 = worksheet.Range($"B{titleRow + 1}:{lastColumnName}{titleRow + 1}").Merge();
+        titleCell1.Value = title;
+        titleCell1.Style.Font.Bold = true;
+        titleCell1.Style.Font.FontSize = 13;
+        titleCell1.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+        titleCell1.Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+        worksheet.Row(titleRow + 1).Height = 20;
+    }
+
+    private void setBody(int counter, StudentRegisterView item, int pos)
+    {
+        var bodyColumn = 2;
+        worksheet!.Cell(counter, bodyColumn++).Value = pos;
+        worksheet.Cell(counter, bodyColumn++).Value = item.studentId; 
+        worksheet.Cell(counter, bodyColumn++).Value = item.minedId.getOrDefault();
+        worksheet.Cell(counter, bodyColumn++).Value = item.educationalLevel; 
+        worksheet.Cell(counter, bodyColumn++).Value = item.degree; 
+        worksheet.Cell(counter, bodyColumn++).Value = item.section;  
+        worksheet.Cell(counter, bodyColumn++).Value = item.fullName; 
+        worksheet.Cell(counter, bodyColumn++).Value = item.birthday.ToString("dd-MM-yyyy");
+        worksheet.Cell(counter, bodyColumn++).Value = item.getAge();  
+        worksheet.Cell(counter, bodyColumn++).Value = item.sex ? "M" : "F"; 
+        worksheet.Cell(counter, bodyColumn++).Value = item.weight; 
+        worksheet.Cell(counter, bodyColumn++).Value = item.height;  
+        worksheet.Cell(counter, bodyColumn++).Value = item.address;  
+        worksheet.Cell(counter, bodyColumn++).Value = item.mother; 
+        worksheet.Cell(counter, bodyColumn++).Value = item.motherIdCard; 
+        worksheet.Cell(counter, bodyColumn++).Value = item.father; 
+        worksheet.Cell(counter, bodyColumn++).Value = item.fatherIdCard;
+        worksheet.Cell(counter, bodyColumn++).Value = item.tutor; 
+        worksheet.Cell(counter, bodyColumn++).Value = item.phone; 
+        worksheet.Cell(counter, bodyColumn++).Value = item.getIsRepetingText();
+        worksheet.Cell(counter, bodyColumn + 1).Value = item.getEnrollDateString();
+    }
+
+    private void setHeader(int headerRow)
+    { 
+        var headerStyle = worksheet!.Range($"B{headerRow}:{lastColumnName}{headerRow}");
+        headerStyle.Style.Font.Bold = true;
+        headerStyle.Style.Fill.BackgroundColor = XLColor.LightGray;
+        headerStyle.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+        headerStyle.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+        
+        var headerColumn = 2;
+        worksheet.Cell(headerRow, headerColumn++).Value = "N°";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Código";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Cód. mined";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Modalidad";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Nivel";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Sección";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Nombre completo";
+        worksheet.Cell(headerRow, headerColumn++).Value = "F. nacimiento";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Edad";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Sexo";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Peso";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Talla";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Dirección";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Madre";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Céd. madre";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Padre";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Céd. padre";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Tutor";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Teléfono";
+        worksheet.Cell(headerRow, headerColumn++).Value = "Repitente";
+        worksheet.Cell(headerRow, headerColumn + 1).Value = "F. matrícula";
     }
 }
