75540d6bd8708ca151603cafc3a6d6423ed5ae57
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-07 21:24:16 -0600

    Feat: add getMetricOfEnrollmentGuide method in ViewEnrollmentGuideActions class
    Refs: (#133) #134

--- src/controller/api/ViewEnrollmentGuideActions.cs
+++ src/controller/api/ViewEnrollmentGuideActions.cs
@@ -5,21 +5,35 @@ using wsmcbl.src.middleware;
 
 namespace wsmcbl.src.controller.api;
 
-[Route("academy")]
+[Route("academy/teachers/{teacherId}/enrollments/guide")]
 [ApiController]
 public class ViewEnrollmentGuideActions(ViewEnrollmentGuideController controller) : ActionsBase
 {
-    /// <summary>Returns the list of active enrollment by teacher.</summary>
-    /// <response code="200">Returns a list, the list can be empty.</response>
+    /// <summary>Returns enrollment guide by teacher.</summary>
+    /// <response code="200">Returns a resource.</response>
     /// <response code="401">If the query was made without authentication.</response>
     /// <response code="403">If the query was made without proper permissions.</response>
-    /// <response code="404">Teacher not found.</response>
+    /// <response code="404">Teacher or enrollment not found.</response>
     [HttpGet]
-    [Route("teachers/{teacherId}/enrollments/temporal")]
+    [Route("")]
     [ResourceAuthorizer("teacher:read")]
     public async Task<IActionResult> getEnrollmentGuide([Required] string teacherId)
     {
         var result = await controller.getEnrollmentGuideByTeacherId(teacherId);
+        return Ok(result);
+    }
+    
+    /// <summary>Returns enrollment guide metrics by teacher.</summary>
+    /// <response code="200">Returns a resource.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    /// <response code="404">Teacher or enrollment not found.</response>
+    [HttpGet]
+    [Route("metrics")]
+    [ResourceAuthorizer("teacher:read")]
+    public async Task<IActionResult> getMetricOfEnrollmentGuide([Required] string teacherId)
+    {
+        var result = await controller.getEnrollmentGuideMetric(teacherId);
         return Ok(result);
     }
 }
