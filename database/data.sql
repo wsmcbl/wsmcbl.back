7c637e0a7260fbd3763674e441e0972891dcdd4e
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-08 21:54:16 -0600

    Update: data.sql file

--- database/data.sql
+++ database/data.sql
@@ -9,10 +9,11 @@ values ('admin', 'Full system access.'),
        ('inspector', 'Access to the student incident module.');
 
 insert into config.user(roleid, name, secondname, surname, secondsurname, email, userstate, createdat, updatedat, password)
-values (4, 'Usuario', 'por', 'Defecto', 'del sistema', 'user.default@cbl-edu.com', true, now(), now(),'AQAAAAIAAYagAAAAEBA+otefABAFYU//4mkRSCB+4Ehre7sDid871rFP7vW3snwji5+cxvjXsWUa1AasZw=='),
+values (4, 'Docente', 'por', 'Defecto', 'del sistema', 'user.default@cbl-edu.com', true, now(), now(),'AQAAAAIAAYagAAAAEBA+otefABAFYU//4mkRSCB+4Ehre7sDid871rFP7vW3snwji5+cxvjXsWUa1AasZw=='),
        (1, 'Kenny', 'Jordan', 'Tinoco', 'Cerda', 'kenny.tinoco@cbl-edu.com', true, now(), now(),'AQAAAAIAAYagAAAAEOKy+ElmYTF+ClhQ68aqO1TCREwarjQMzylachhHEo0/duwGTqkAf5IWQRQeNdEH+g=='),
-       (1, 'Ezequiel', 'De jesús', 'Urbina', 'Zeledón', 'ezequiel.urbina@cbl-edu.com', true, now(), now(),'AQAAAAIAAYagAAAAEK/ObbY+PMQMXK/Q2rqJQyZKPUwiZGPALh/Bww0t6j9gozilS/PVoYQfLo8eDoHmFA=='),
-       (1, 'Thelma', null, 'Ríos', 'Zeas', 'thelma.rios@cbl-edu.com', true, now(), now(),'AQAAAAIAAYagAAAAEMexOu51jrRZOshRdeF0yZ5wm3HCcmvzJLuZI1aIeX1h4Mcfwx6BbHJMi+UyaZlUDA==');
+       (1, 'Admin', 'por', 'Defecto',null,'admin@cbl-edu.com', true, now(), now(),'AQAAAAIAAYagAAAAEB74WP4DVQ7eME5thSW/ObX5yiT0jyJKmv/vPC9lO3DEZFPzD5yO9s3AReuMfaLvxw=='),
+       (3, 'Ezequiel', 'De jesús', 'Urbina', 'Zeledón', 'ezequiel.urbina@cbl-edu.com', true, now(), now(),'AQAAAAIAAYagAAAAEK/ObbY+PMQMXK/Q2rqJQyZKPUwiZGPALh/Bww0t6j9gozilS/PVoYQfLo8eDoHmFA=='),
+       (3, 'Thelma', null, 'Ríos', 'Zeas', 'thelma.rios@cbl-edu.com', true, now(), now(),'AQAAAAIAAYagAAAAEMexOu51jrRZOshRdeF0yZ5wm3HCcmvzJLuZI1aIeX1h4Mcfwx6BbHJMi+UyaZlUDA==');
 
 
 
@@ -102,10 +103,7 @@ values ('Mensualidad'),
 INSERT INTO Accounting.cashier(userid)
 SELECT u.userid FROM config.user u WHERE u.name = 'Ezequiel';
 
-INSERT INTO Accounting.cashier(userid)
-SELECT u.userid FROM config.user u WHERE u.name = 'Kenny';
-
-INSERT INTO academy.teacher(userid, isguide)
+INSERT INTO Academy.teacher(userid, isguide)
 SELECT u.userid, false
 FROM config.user u
-WHERE u.name = 'Usuario';
+WHERE u.name = 'Docente';
