32f60ebac3bf5b64aba6003fd5569742f926cad4
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-09 21:55:12 -0600

    Feat: add getAge method in StudentEntity class
    Refs: (#133) #136

--- src/dto/academy/BasicGuideStudentDto.cs
+++ src/dto/academy/BasicGuideStudentDto.cs
@@ -7,7 +7,7 @@ public class BasicGuideStudentDto
     public string studentId { get; set; }
     public string minedId { get; set; }
     public string fullName { get; set; }
-    public string age { get; set; }
+    public int age { get; set; }
     public bool sex { get; set; }
 
     public BasicGuideStudentDto(StudentEntity value)
