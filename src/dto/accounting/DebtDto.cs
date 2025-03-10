349c0d84c5c65cc4415629fb27fa36b7a80b29cb
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-10 12:11:43 -0600

    Fix: DebtDto constructor

--- src/dto/accounting/DebtDto.cs
+++ src/dto/accounting/DebtDto.cs
@@ -7,32 +7,23 @@ public class DebtDto
     public int tariffId { get; set; }
     public string? schoolYear { get; set; }
     public string? concept { get; set; }
-    public float total { get; set; }
-    public float discount { get; set; }
-    public float arrears { get; set; }
     public float subTotal { get; set; }
+    public float arrears { get; set; }
+    public float discount { get; set; }
+    public float total { get; set; }
     public float debtBalance { get; set; }
     public bool itPaid { get; set; }
-
-    public DebtDto()
-    {
-    }
 
     public DebtDto(DebtHistoryEntity entity)
     {
         tariffId = entity.tariffId;
         schoolYear = entity.schoolyear;
         concept = entity.tariff.concept;
-        total = entity.amount;
-        arrears = entity.arrears;
         subTotal = entity.subAmount;
-        itPaid = entity.isPaid;
+        discount = subTotal != 0 ? entity.tariff.amount - subTotal : 0;
+        arrears = entity.arrears;
+        total = entity.amount;
         debtBalance = entity.getDebtBalance();
-        setDiscount(entity.subAmount);
-    }
-    
-    private void setDiscount(float value)
-    {
-        discount = total - value;
+        itPaid = entity.isPaid;
     }
 }
