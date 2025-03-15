75ba29f03f5233673f66c2354c992166137dad3a
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-15 10:13:12 -0600

    Update: TariffDataEntity class
    Refs: (#138) #141

--- src/model/secretary/TariffDataEntity.cs
+++ src/model/secretary/TariffDataEntity.cs
@@ -5,7 +5,8 @@ public class TariffDataEntity
     public int tariffDataId { get; set; }
     public int typeId { get; set; } 
     public string concept { get; set; } = null!;
-    public float amount { get; set; }
+    public decimal amount { get; set; }
     public DateOnly? dueDate { get; set; }
     public int educationalLevel { get; set; }
+    public bool isActive { get; set; }
 }
