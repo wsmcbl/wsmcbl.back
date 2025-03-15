b6c27fbffc28a2d3c25c57bcd3fab0a8c7341967
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-14 21:22:04 -0600

    Feat: add getExchangeRateList and updateCurrentExchangeRate methods in CreateSchoolyearActions class
    Refs: (#138) #139

--- src/controller/api/CreateSchoolyearActions.cs
+++ src/controller/api/CreateSchoolyearActions.cs
@@ -55,4 +55,31 @@ public class CreateSchoolyearActions(CreateSchoolyearController controller) : Co
         var result = controller.getSchoolyearCreated();
         return CreatedAtAction(null, result.mapToDto());
     }
+    
+    /// <summary>Returns the list of exchange rate.</summary>
+    /// <response code="200">Returns a list, the list can be empty.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    [HttpGet]
+    [Route("rates")]
+    [ResourceAuthorizer("schoolyear:read")]
+    public async Task<IActionResult> getExchangeRateList()
+    {
+        return Ok(await controller.getExchangeRateList());
+    }
+    
+    /// <summary>Update current exchange rate.</summary>
+    /// <response code="200">If the resource was modified correctly.</response>
+    /// <response code="400">Parameter is not valid.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    /// <response code="404">Resource not found.</response>
+    [HttpPut]
+    [Route("rates/new")]
+    [ResourceAuthorizer("schoolyear:read")]
+    public async Task<IActionResult> updateCurrentExchangeRate([FromQuery] decimal exchange)
+    {
+        await controller.updateCurrentExchangeRate(exchange);
+        return Ok();
+    }
 }
