184ff38e4c1b067f68432b04648f4304808d21c5
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-27 13:08:15 -0600

    Feat: add getSummaryTeacherGrades method in ViewDirectorDashboardController class
    Refs: (#143) #144

--- src/controller/api/ViewDirectorDashboardActions.cs
+++ src/controller/api/ViewDirectorDashboardActions.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using wsmcbl.src.controller.business;
 
 namespace wsmcbl.src.controller.api;
 
@@ -6,14 +7,25 @@ namespace wsmcbl.src.controller.api;
 [ApiController]
 public class ViewDirectorDashboardActions(ViewDirectorDashboardController controller) : ActionsBase
 {
-    /// <summary>Get enrolled students.</summary>
+    /// <summary>Get summary of the students for current schoolyear.</summary>
     /// <response code="200">Return the value</response>
     /// <response code="401">If the query was made without authentication.</response>
     /// <response code="403">If the query was made without proper permissions.</response>
     [HttpGet]
-    [Route("permissions")]
-    public async Task<IActionResult> getPermissionList()
+    [Route("students/summaries")]
+    public async Task<IActionResult> getSummaryStudentQuantity()
     {
-        return Ok(await controller.getEnrolledStudent());
+        return Ok(await controller.getSummaryStudentQuantity());
+    }
+    
+    /// <summary>Get summary of the teachers who entered grades.</summary>
+    /// <response code="200">Return the value</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    [HttpGet]
+    [Route("teachers/grades/summaries")]
+    public async Task<IActionResult> getSummaryTeacherGrades()
+    {
+        return Ok(await controller.getSummaryTeacherGrades());
     }
 }
