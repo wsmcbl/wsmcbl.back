3dfb4d6905a93584cfaec8806c9554bcc781c6df
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-27 14:43:40 -0600

    Feat: add setSummaryStudentByArea and setSummaryStudentByDegree methods in ViewDirectorDashboardController class
    Refs: (#143) #144

--- src/controller/business/ViewDirectorDashboardController.cs
+++ src/controller/business/ViewDirectorDashboardController.cs
@@ -1,14 +1,44 @@
 using wsmcbl.src.model.dao;
+using wsmcbl.src.model.secretary;
 
 namespace wsmcbl.src.controller.business;
 
 public class ViewDirectorDashboardController(DaoFactory daoFactory) : BaseController(daoFactory)
 {
-    public async Task<object?> getSummaryStudentQuantity()
+    private List<StudentRegisterView> studentList { get; set; } = null!;
+    
+    public async Task<SummaryStudentDto> getSummaryStudentQuantity()
     {
-        await Task.CompletedTask;
-        return 0;
+        studentList = await daoFactory.studentDao!.getStudentRegisterListForCurrentSchoolyear();
+        
+        var total = studentList.Count;
+        var males = studentList.Count(e => e.sex);
+        return new SummaryStudentDto(total, males, 0);
     }
+
+    private async Task setSummaryStudentByArea(SummaryStudentDto dto)
+    {
+        var list = studentList.Where(e => e.educationalLevel!.Equals("Preescolar")).ToList();
+        var area1 = new SummaryByAreaDto(list.Count, list.Count(e => e.sex));
+        dto.areaList.Add(area1);
+        
+        list = studentList.Where(e => e.educationalLevel!.Equals("Primaria")).ToList();
+        var area2 = new SummaryByAreaDto(list.Count, list.Count(e => e.sex));
+        dto.areaList.Add(area2);
+        
+        list = studentList.Where(e => e.educationalLevel!.Equals("Secundaria")).ToList();
+        var area3 = new SummaryByAreaDto(list.Count, list.Count(e => e.sex));
+        dto.areaList.Add(area3);
+    }
+
+    private async Task setSummaryStudentByDegree(SummaryStudentDto dto)
+    {
+        var schoolyear = await daoFactory.schoolyearDao!.getCurrentOrNew();
+        var degreeList = await daoFactory.degreeDao!.getListForSchoolyearId(schoolyear.id!);
+
+        degreeList = degreeList.OrderBy(e => e.educationalLevel).ThenBy(e => e.tag).ToList();
+    }
+    
 
     public async Task<object?> getSummaryTeacherGrades()
     {
