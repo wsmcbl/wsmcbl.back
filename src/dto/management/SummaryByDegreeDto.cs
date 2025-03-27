82b0bae2693b69848decd0b122f013c3ebc73fd9
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-27 15:22:09 -0600

    Update: SummaryByDegreeDto class
    Refs: (#143) #145

--- src/dto/management/SummaryByDegreeDto.cs
+++ src/dto/management/SummaryByDegreeDto.cs
@@ -2,13 +2,15 @@ namespace wsmcbl.src.dto.management;
 
 public class SummaryByDegreeDto
 {
+    public int level { get; set; }
     public int position { get; set; }
     public int total { get; set; }
     public int males { get; set; }
     public string label { get; set; }
 
-    public SummaryByDegreeDto(string label, int position, int total, int males)
+    public SummaryByDegreeDto(string label, int position, int level, int total, int males)
     {
+        this.level = level;
         this.label = label;
         this.position = position;
         this.total = total;
