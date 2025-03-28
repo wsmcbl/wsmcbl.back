69621d09d919d9450d66180ddd885fdbab412020
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-28 11:07:56 -0600

    Feat: add mapListToDto method in DtoMapper class
    Refs: (#143) #145

--- src/dto/management/DtoMapper.cs
+++ src/dto/management/DtoMapper.cs
@@ -1,4 +1,6 @@
 using wsmcbl.src.model.academy;
+using wsmcbl.src.model.secretary;
+using SubjectEntity = wsmcbl.src.model.secretary.SubjectEntity;
 
 namespace wsmcbl.src.dto.management;
 
@@ -9,4 +11,22 @@ public static class DtoMapper
     
     public static List<TeacherReportDto> mapListToDto(this List<TeacherEntity> list) =>
         list.Select(e => new TeacherReportDto(e)).ToList();
+
+    public static List<SubjectNameDto> mapListToDto(this List<SubjectEntity> subjectList, List<DegreeEntity> degreeList)
+    {
+        var result = new List<SubjectNameDto>();
+
+        foreach (var item in subjectList)
+        {
+            var degree = degreeList.FirstOrDefault(e => e.degreeId == item.degreeId);
+            if (degree == null)
+            {
+                continue;
+            }
+            
+            result.Add(new SubjectNameDto(item, degree));
+        }
+
+        return result;
+    }
 }
