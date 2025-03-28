60848073fbe39d28a03094d833882a0eb61fc2d5
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-27 21:25:53 -0600

    Feat: add SubjectGradedView class
    Refs: (#143) #146

---
+++ src/model/academy/SubjectGradedView.cs
@@ -0,0 +1,9 @@
+namespace wsmcbl.src.model.academy;
+
+public class SubjectGradedView
+{
+    public string subjectId { get; set; } = null!;
+    public string teacherId { get; set; } = null!;
+    public int partialId { get; set; }
+    public int studentCount { get; set; }
+}
