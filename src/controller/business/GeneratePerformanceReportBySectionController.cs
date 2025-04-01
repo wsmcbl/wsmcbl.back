ecb55b6da9c5f987ec6b1aabe9383c0b66666051
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-01 08:25:28 -0600

    Update: getEnrollmentPerformanceByTeacherId method in GeneratePerformanceReportBySectionController class
    Refs: (#148) #149

--- src/controller/business/GeneratePerformanceReportBySectionController.cs
+++ src/controller/business/GeneratePerformanceReportBySectionController.cs
@@ -1,3 +1,5 @@
+using wsmcbl.src.exception;
+using wsmcbl.src.model.academy;
 using wsmcbl.src.model.dao;
 
 namespace wsmcbl.src.controller.business;
@@ -8,8 +10,22 @@ public class GeneratePerformanceReportBySectionController : BaseController
     {
     }
 
-    public async Task<object?> getEnrollmentPerformanceByTeacherId(string teacherId)
-    {
-        return await daoFactory.teacherDao!.getById(teacherId);
+    public async Task<List<StudentEntity>> getEnrollmentPerformanceByTeacherId(string teacherId)
+    { 
+        var teacher = await daoFactory.teacherDao!.getById(teacherId);
+        if (teacher == null)
+        {
+            throw new EntityNotFoundException("TeacherEntity", teacherId);
+        }
+
+        await teacher.setCurrentEnrollment(daoFactory.schoolyearDao!);
+        if (!teacher.hasCurrentEnrollment())
+        {
+            throw new EntityNotFoundException($"Entity of type (EnrollmentEntity) with teacher id ({teacherId}) not found.");
+        }
+
+        var enrollment = await daoFactory.enrollmentDao!.getFullById(teacher.getCurrentEnrollmentId());
+        
+        return enrollment.studentList!.ToList();
     }
 }
