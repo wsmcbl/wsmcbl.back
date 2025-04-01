2451d594205053ad9376a523f142c15c2a07546c
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-01 09:20:56 -0600

    Feat: add conductGrade property in GradeView class
    Refs: (#148) #151

--- src/model/academy/GradeView.cs
+++ src/model/academy/GradeView.cs
@@ -10,5 +10,6 @@ public class GradeView
     public string schoolyearId { get; set; } = null!;
     public int partial { get; set; }
     public decimal grade { get; set; }
+    public decimal conductGrade { get; set; }
     public string label { get; set; } = null!;
 }
