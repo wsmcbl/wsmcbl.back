7429a851ca233220181c0e6b05bf548340ad5d3e
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-01 11:37:51 -0600

    Feat: StudentGradeViewDto class
    Refs: (#148) #150

--- src/dto/academy/StudentGradeSummaryDto.cs
+++ src/dto/academy/StudentGradeSummaryDto.cs
@@ -2,22 +2,22 @@ using wsmcbl.src.model.academy;
 
 namespace wsmcbl.src.dto.academy;
 
-public class StudentGradeDto
+public class StudentGradeSummaryDto
 {
     public string studentId { get; set; }
     public string minedId { get; set; }
     public string fullName { get; set; }
     public bool sex { get; set; }
-    public List<GradeViewDto> averageList {get; set;}
+    public List<StudentGradeViewDto> averageList {get; set;}
     public decimal grade { get; set; }
     public decimal label { get; set; }
     public decimal conductGrade { get; set; }
 
-    public StudentGradeDto(StudentEntity parameter, int partial)
+    public StudentGradeSummaryDto(StudentEntity parameter, int partial)
     {
         studentId = parameter.studentId;
         fullName = parameter.fullName();
-        averageList = parameter.gradeList.Select(e => new GradeViewDto(e)).ToList();
+        averageList = parameter.gradeList!.Select(e => new StudentGradeViewDto(e)).ToList();
         
         var average = parameter.getAverage(partial);
         grade = average.grade;
