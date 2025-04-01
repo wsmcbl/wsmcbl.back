da91e6a7c8022d39215b8e48a3f989aab8046732
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-03-31 21:45:47 -0600

    Feat: add GeneratePerformanceReportBySectionActions class
    Refs: (#148) #149

---
+++ src/controller/api/GeneratePerformanceReportBySectionActions.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using wsmcbl.src.controller.business;
+using wsmcbl.src.middleware;
+
+namespace wsmcbl.src.controller.api;
+
+[ApiController]
+[Route("academy/teachers/{teacherId}/enrollments/guide")]
+[Authorizer("teacher:enrollment:guide")]
+public class GeneratePerformanceReportBySectionActions(GeneratePerformanceReportBySectionController controller) : ActionsBase 
+{
+    /// <summary>Returns performance enrollment guide by teacher.</summary>
+    /// <response code="200">Returns a resource.</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    /// <response code="404">Teacher or enrollment not found.</response>
+    [HttpGet]
+    [Route("performance")]
+    public async Task<IActionResult> getPerformanceEnrollmentGuide([Required] string teacherId)
+    {
+        var result = await controller.getPerformanceEnrollmentGuideByTeacherId(teacherId);
+        return Ok(result.mapToDto());
+    }
+}
