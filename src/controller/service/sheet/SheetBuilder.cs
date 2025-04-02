8fdff971589113528f1841630a1b7dddebd69fdb
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-02 09:45:59 -0600

    Update: SheetBuilder class
    Refs: (#148) #149

--- src/controller/service/sheet/SheetBuilder.cs
+++ src/controller/service/sheet/SheetBuilder.cs
@@ -1,10 +1,16 @@
 using ClosedXML.Excel;
+using wsmcbl.src.utilities;
 
 namespace wsmcbl.src.controller.service.sheet;
 
-public class SheetBuilder
+public abstract class SheetBuilder
 {
+    protected int columnQuantity { get; set; }
     protected IXLWorksheet worksheet { get; set; } = null!;
+    protected string lastColumnName { get; set; } = null!;
+    
+    protected abstract string updateContent(string value);
+    protected abstract void setColumnQuantity();
     
     protected static string getColumnName(int value)
     {
@@ -17,5 +23,23 @@ public class SheetBuilder
         }
 
         return result;
+    }
+    
+    private void setBorder(int lastRow, int headerRow)
+    {
+        var tableRange = worksheet!.Range($"B{headerRow}:{lastColumnName}{lastRow}");
+
+        tableRange.Style.Border.TopBorder = XLBorderStyleValues.Thin;
+        tableRange.Style.Border.BottomBorder = XLBorderStyleValues.Thin;
+        tableRange.Style.Border.LeftBorder = XLBorderStyleValues.Thin;
+        tableRange.Style.Border.RightBorder = XLBorderStyleValues.Thin;
+        tableRange.Style.Border.InsideBorder = XLBorderStyleValues.Thin;
+    }
+    
+    private void setDate(int row, string userAlias)
+    {
+        var dateCell = worksheet!.Range($"B{row}:{lastColumnName}{row}").Merge();
+        dateCell.Value = $"Generado por wsmcbl el {DateTime.UtcNow.toStringUtc6()}, {userAlias}.";
+        dateCell.Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
     }
 }
