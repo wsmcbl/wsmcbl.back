674cf5c98c09c789bb288d6c018550db1c9d1630
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-03 11:13:53 -0600

    Update: DistributionSummaryDto class
    Refs: (#153) #156

--- src/dto/academy/DistributionSummaryDto.cs
+++ src/dto/academy/DistributionSummaryDto.cs
@@ -10,13 +10,12 @@ public class DistributionSummaryDto
     public EvaluationStatsDto AI { get; set; }
     public EvaluationStatsDto quantity { get; set; }
     
-    public DistributionSummaryDto(List<StudentEntity> parameter)
+    public DistributionSummaryDto(List<StudentEntity> parameter, int partial)
     {
-        
-        AA = new EvaluationStatsDto(parameter.Where(e => e.hasAverageRange(1)));
-        AS = new EvaluationStatsDto(parameter.Where(e => e.hasAverageRange(2)));
-        AF = new EvaluationStatsDto(parameter.Where(e => e.hasAverageRange(3)));
-        AI = new EvaluationStatsDto(parameter.Where(e => e.hasAverageRange(4)));
+        AA = new EvaluationStatsDto(parameter.Where(e => e.isWithInRange("AA", partial)).ToList());
+        AS = new EvaluationStatsDto(parameter.Where(e => e.isWithInRange("AS", partial)).ToList());
+        AF = new EvaluationStatsDto(parameter.Where(e => e.isWithInRange("AF", partial)).ToList());
+        AI = new EvaluationStatsDto(parameter.Where(e => e.isWithInRange("AI", partial)).ToList());
         
         quantity = new EvaluationStatsDto(parameter);
     }
