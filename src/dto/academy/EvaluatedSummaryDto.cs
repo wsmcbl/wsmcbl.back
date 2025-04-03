65f147fd62b2076d2cfca65d98564aae8a7c96f5
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-03 09:48:11 -0600

    Update: EvaluationSummaryDto class
    Refs: (#153) #155

--- src/dto/academy/EvaluatedSummaryDto.cs
+++ src/dto/academy/EvaluatedSummaryDto.cs
@@ -4,10 +4,24 @@ namespace wsmcbl.src.dto.academy;
 
 public class EvaluatedSummaryDto
 {
+    public EvaluationStatsDto initialQuantity { get; set; }
     public EvaluationStatsDto currentQuantity { get; set; }
+    public EvaluationStatsDto approved { get; set; }
+    public EvaluationStatsDto failedFromOneToTwo { get; set; }
+    public EvaluationStatsDto failedFromThreeToMore { get; set; }
 
-    public EvaluatedSummaryDto(List<StudentEntity> parameter)
+    public EvaluatedSummaryDto(List<StudentEntity> parameter, List<StudentEntity> intial, int partial)
     {
-        currentQuantity = new EvaluationStatsDto();
+        initialQuantity = new EvaluationStatsDto(intial.Count, intial.Count(e => e.student.sex));
+        currentQuantity = new EvaluationStatsDto(parameter.Count, parameter.Count(e => e.student.sex));
+
+        var approvedList = parameter.Where(e => e.isApproved(partial)).ToList();
+        approved = new EvaluationStatsDto(approvedList.Count, approvedList.Count(e => e.student.sex));
+        
+        var failedFromOneToTwoList = parameter.Where(e => e.isFailed(1)).ToList();
+        failedFromOneToTwo = new EvaluationStatsDto(failedFromOneToTwoList.Count, failedFromOneToTwoList.Count(e => e.student.sex));
+        
+        var failedFromThreeToMoreList = parameter.Where(e => e.isFailed(2)).ToList();
+        failedFromThreeToMore = new EvaluationStatsDto(failedFromThreeToMoreList.Count, failedFromThreeToMoreList.Count(e => e.student.sex));
     }
 }
