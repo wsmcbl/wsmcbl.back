36d0644a2b8e42926935eff634f93447a466301d
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-03 09:26:31 -0600

    Update: EvaluationStatsDto class
    Refs: (#153) #155

--- src/dto/academy/EvaluationStatsDto.cs
+++ src/dto/academy/EvaluationStatsDto.cs
@@ -5,4 +5,11 @@ public class EvaluationStatsDto
     public int total { get; set; }
     public int males { get; set; }
     public int females { get; set; }
+
+    public EvaluationStatsDto(int parameter, int males)
+    {
+        total = parameter;
+        this.males = males;
+        females = parameter - males;
+    }
 }
