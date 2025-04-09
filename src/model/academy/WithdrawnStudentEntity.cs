cc6e62561b35ee6e56c2c76772b1ebc805b21bc0
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-09 08:32:28 -0600

    Update: override getAll method in WithdrawnStudentDaoPostgres class
    Refs: (#158) #161

--- src/model/academy/WithdrawnStudentEntity.cs
+++ src/model/academy/WithdrawnStudentEntity.cs
@@ -19,7 +19,7 @@ public class WithdrawnStudentEntity
     public WithdrawnStudentEntity(StudentEntity parameter)
     {
         studentId = parameter.studentId;
-        lastEnrollmentId = parameter.studentId;
+        lastEnrollmentId = parameter.enrollmentId!;
         schoolyearId = parameter.schoolyearId;
         enrolledAt = parameter.createdAt;
         withdrawnAt = DateTime.UtcNow;
