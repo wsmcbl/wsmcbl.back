7621b6dbb628bd8b47f318bc7d621c88d16b1ff0
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-08 21:16:10 -0600

    Feat: add getAllForCurrentSchoolyear method in IWithdrawnStudentDao interface
    Refs: (#143) #146

--- src/database/WithdrawnStudentDaoPostgres.cs
+++ src/database/WithdrawnStudentDaoPostgres.cs
@@ -5,8 +5,7 @@ using wsmcbl.src.model.academy;
 
 namespace wsmcbl.src.database;
 
-public class WithdrawnStudentDaoPostgres(PostgresContext context)
-    : GenericDaoPostgres<WithdrawnStudentEntity, int>(context), IWithdrawnStudentDao
+public class WithdrawnStudentDaoPostgres(PostgresContext context) : GenericDaoPostgres<WithdrawnStudentEntity, int>(context), IWithdrawnStudentDao
 {
     public async Task<List<WithdrawnStudentEntity>> getListByEnrollmentId(string enrollmentId, bool hasBeforeFirstPartial = false)
     {
@@ -28,5 +27,13 @@ public class WithdrawnStudentDaoPostgres(PostgresContext context)
 
         return await query.Where(e => firstPartial.startDate >= DateOnly.FromDateTime(e.enrolledAt))
             .Include(e => e.student).ToListAsync();
+    }
+
+    public async Task<List<WithdrawnStudentEntity>> getAllForCurrentSchoolyear()
+    {
+        var daoFactory = new DaoFactoryPostgres(context);
+        var currentSchoolyear = await daoFactory.schoolyearDao.getCurrent();
+
+        return await entities.Where(e => e.schoolyearId == currentSchoolyear.id).ToListAsync();
     }
 }
