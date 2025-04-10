81391f20f26aa021ba93b979b874382c6848bf46
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-10 11:52:16 -0600

    Update: AmountByStudentQuantityDto class
    Refs: (#169) #171

--- src/dto/accounting/AmountByStudentQuantityDto.cs
+++ src/dto/accounting/AmountByStudentQuantityDto.cs
@@ -1,7 +1,15 @@
+using wsmcbl.src.model.accounting;
+
 namespace wsmcbl.src.dto.accounting;
 
 public class AmountByStudentQuantityDto
 {
     public decimal amount { get; set; }
     public int studentQuantity { get; set; }
+
+    public AmountByStudentQuantityDto(List<DebtHistoryEntity> parameter)
+    {
+        amount = parameter.Sum(e => e.amount);
+        studentQuantity = parameter.Select(e => e.studentId).Distinct().Count();
+    }
 }
