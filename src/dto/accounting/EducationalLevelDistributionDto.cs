3273efb0842ab5c35f345537154db5d384b2b337
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-10 11:52:35 -0600

    Update: EducationalLevelDistributionDto class
    Refs: (#169) #171

--- src/dto/accounting/EducationalLevelDistributionDto.cs
+++ src/dto/accounting/EducationalLevelDistributionDto.cs
@@ -1,10 +1,26 @@
+using wsmcbl.src.model.accounting;
+
 namespace wsmcbl.src.dto.accounting;
 
 public class EducationalLevelDistributionDto
 {
     public decimal tariffAmount { get; set; }
-    public AmountByStudentQuantityDto total { get; set; } = null!;
-    public AmountByStudentQuantityDto regularStudent { get; set; } = null!;
-    public AmountByStudentQuantityDto discountedStudent { get; set; } = null!;
-    public AmountByStudentQuantityDto scholarshipStudent { get; set; } = null!;
+    public AmountByStudentQuantityDto total { get; set; }
+    public AmountByStudentQuantityDto regularStudent { get; set; }
+    public AmountByStudentQuantityDto discountedStudent { get; set; }
+    public AmountByStudentQuantityDto scholarshipStudent { get; set; }
+
+    public EducationalLevelDistributionDto(List<DebtHistoryEntity> parameter)
+    {
+        var first = parameter.FirstOrDefault();
+        if (first != null)
+        {
+            tariffAmount = first.tariff.amount;
+        }
+
+        total = new AmountByStudentQuantityDto(parameter);
+        regularStudent = new AmountByStudentQuantityDto(parameter);
+        discountedStudent = new AmountByStudentQuantityDto(parameter);
+        scholarshipStudent = new AmountByStudentQuantityDto(parameter);
+    }
 }
