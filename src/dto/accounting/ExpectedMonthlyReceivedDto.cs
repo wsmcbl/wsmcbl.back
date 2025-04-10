8e854a34d179afde9e0802279ca05e6fc884d149
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-10 12:01:05 -0600

    Update: ExpectedMonthlyReceivedDto class
    Refs: (#169) #171

--- src/dto/accounting/ExpectedMonthlyReceivedDto.cs
+++ src/dto/accounting/ExpectedMonthlyReceivedDto.cs
@@ -1,9 +1,25 @@
+using wsmcbl.src.model.accounting;
+
 namespace wsmcbl.src.dto.accounting;
 
 public class ExpectedMonthlyReceivedDto
 {
-    public AmountByStudentQuantityDto total { get; set; } = null!;
-    public EducationalLevelDistributionDto preschool { get; set; } = null!;
-    public EducationalLevelDistributionDto elementary { get; set; } = null!;
-    public EducationalLevelDistributionDto secondary { get; set; } = null!;
+    public AmountByStudentQuantityDto total { get; set; }
+    public EducationalLevelDistributionDto preschool { get; set; }
+    public EducationalLevelDistributionDto elementary { get; set; }
+    public EducationalLevelDistributionDto secondary { get; set; }
+
+    public ExpectedMonthlyReceivedDto(List<DebtHistoryEntity> parameter)
+    {
+        total = new AmountByStudentQuantityDto(parameter);
+
+        var list = parameter.Where(e => e.tariff.educationalLevel == 1).ToList();
+        preschool = new EducationalLevelDistributionDto(list);
+        
+        list = parameter.Where(e => e.tariff.educationalLevel == 2).ToList();
+        elementary = new EducationalLevelDistributionDto(list);
+        
+        list = parameter.Where(e => e.tariff.educationalLevel == 3).ToList();
+        secondary = new EducationalLevelDistributionDto(list);
+    }
 }
