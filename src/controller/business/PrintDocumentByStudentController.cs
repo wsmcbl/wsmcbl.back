035d6c4984eb450c54fec6c375bf283a73207d38
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-18 16:48:01 -0600

    Feat: add getActiveCertificateDocument method in PrintDocumentByStudentController class
    Refs: (#174) #175

--- src/controller/business/PrintDocumentByStudentController.cs
+++ src/controller/business/PrintDocumentByStudentController.cs
@@ -3,11 +3,16 @@ using wsmcbl.src.model.dao;
 
 namespace wsmcbl.src.controller.business;
 
-public class PrintDocumentByStudentController(DaoFactory daoFactory) : BaseController(daoFactory)
+public class PrintDocumentByStudentController : BaseController
 {
+    private DocumentMaker documentMaker { get; set; }
+    public PrintDocumentByStudentController(DaoFactory daoFactory) : base(daoFactory)
+    {
+        documentMaker = new DocumentMaker(daoFactory);   
+    }
+    
     public async Task<byte[]> getReportCard(string studentId, string userId)
     {
-        var documentMaker = new DocumentMaker(daoFactory);
         return await documentMaker.getReportCardByStudent(studentId, userId);
     }
 
@@ -17,5 +22,10 @@ public class PrintDocumentByStudentController(DaoFactory daoFactory) : BaseContr
         
         var debt = debtHistoryList.FirstOrDefault(e => e.isCurrentTariffMonthly());
         return debt != null && debt.isPaid;
+    }
+
+    public async Task<byte[]> getActiveCertificateDocument(string studentId, string userId)
+    {
+        return await documentMaker.getActiveCertificateByStudent(studentId, userId);
     }
 }
