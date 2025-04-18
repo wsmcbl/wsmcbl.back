7c778b57f8525f8bd0bdbdcfcf57d7d137e7d7ce
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-04-18 16:51:34 -0600

    Feat: add getActiveCertificateByStudent method in DocumentMaker class
    Refs: (#174) #175

--- src/controller/service/document/DocumentMaker.cs
+++ src/controller/service/document/DocumentMaker.cs
@@ -124,4 +124,23 @@ public class DocumentMaker(DaoFactory daoFactory) : PdfMaker
         setLatexBuilder(latexBuilder);
         return getPDF();
     }
+
+    public async Task<byte[]> getActiveCertificateByStudent(string studentId, string userId)
+    {
+        var student = await daoFactory.academyStudentDao!.getCurrentById(studentId);
+        var enrollment = await daoFactory.enrollmentDao!.getById(student.enrollmentId!);
+        var schoolyear = await daoFactory.schoolyearDao!.getCurrent();
+        
+        var user = await daoFactory.userDao!.getById(userId);
+
+        var latexBuilder = new ActiveCertificateLatexBuilder.Builder(resource, $"{resource}/out/active")
+            .withStudent(student)
+            .withUserAlias(user.getAlias())
+            .withEnrollment(enrollment!.label)
+            .withSchoolyear(schoolyear.label)
+            .build();
+
+        setLatexBuilder(latexBuilder);
+        return getPDF();
+    }
 }
