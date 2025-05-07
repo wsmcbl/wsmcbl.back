34878dc8b39ee681be68fd103912a13df6f3e85a
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-05-06 22:52:39 -0600

    Feat: add tutors endpoints

--- src/model/secretary/StudentTutorEntity.cs
+++ src/model/secretary/StudentTutorEntity.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace wsmcbl.src.model.secretary;
 
 public class StudentTutorEntity
@@ -29,5 +31,23 @@ public class StudentTutorEntity
         name = entity.name;
         phone = entity.phone;
         email = entity.email;
+    }
+    
+    /// <summary>
+    /// Check if there are multiple or a single phone number with its country code in the string.
+    /// Examples
+    /// +506 68451271
+    /// 7845361
+    /// 7845361, +50578451236
+    /// </summary>
+    public bool isValidPhone()
+    {
+        if (string.IsNullOrWhiteSpace(phone) || phone.Length < 7)
+        {
+            return false;
+        }
+        
+        var value = phone[..8];
+        return Regex.IsMatch(value, @"(?:\+\d{1,3}\s?)?\d{6,14}(?:,\s*(?:\+\d{1,3}\s?)?\d{6,14})*");
     }
 }
