c99cbe950c104bc98adf0e9c69a53ebe9a215d01
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-05-12 21:37:48 -0600

    Feat: add ViewTeacherDashboardActions class
    Refs: (#182) #183

---
+++ src/controller/api/ViewTeacherDashboardActions.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using wsmcbl.src.controller.business;
+using wsmcbl.src.middleware;
+
+namespace wsmcbl.src.controller.api;
+
+[Route("academy")]
+[ApiController]
+public class ViewTeacherDashboardActions(ViewTeacherDashboardController controller) : ActionsBase
+{
+    /// <summary>Get summary of the subject for current schoolyear.</summary>
+    /// <response code="200">Return the value</response>
+    /// <response code="401">If the query was made without authentication.</response>
+    /// <response code="403">If the query was made without proper permissions.</response>
+    [HttpGet]
+    [Route("teachers/{teacherId}/subjects")]
+    [Authorizer("report:teacher:read")]
+    public async Task<IActionResult> getSummarySubjects(string teacherId)
+    {
+        var result = await controller.getSummarySubject();
+        return Ok(result);
+    }
+}
