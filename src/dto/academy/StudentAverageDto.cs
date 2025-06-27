808d41685961ed87153e5346150b4c143c353d1a
kenny-tinoco <kenny.tinoco10@gmail.com>
2025-06-27 11:20:57 -0600

    Update: rename averageGrade property in StudentAverageDto class

--- src/dto/academy/StudentAverageDto.cs
+++ src/dto/academy/StudentAverageDto.cs
@@ -8,13 +8,13 @@ public class StudentAverageDto
     public string studentId { get; set; }
     public string fullName { get; set; }
     public List<GradeAverageDto> averageList {get; set;}
-    public decimal? finalGrade { get; set; }
+    public decimal? averageGrade { get; set; }
 
     public StudentAverageDto(StudentEntity parameter)
     {
         studentId = parameter.studentId;
         fullName = parameter.fullName();
         averageList = parameter.averageList!.Select(e => new GradeAverageDto(e)).ToList();
-        finalGrade = parameter.computeAverageGrade().round();
+        averageGrade = parameter.computeAverageGrade().round();
     }
 }
